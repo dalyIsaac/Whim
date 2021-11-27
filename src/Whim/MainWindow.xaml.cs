d8750041b7cc94c24fc2bcd1548d1157b8e7afc3
dalyIsaac <isaac.daly@outlook.com>
2021-11-28 11:35:36 +1300

    Switched to WPF

--- src/Whim/MainWindow.xaml.cs
+++ src/Whim/MainWindow.xaml.cs
@@ -1,35 +1,26 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Whim;
-
 /// <summary>
-/// An empty window that can be used on its own or navigated to within a Frame.
+/// Interaction logic for MainWindow.xaml
 /// </summary>
-public sealed partial class MainWindow : Window
+public partial class MainWindow : Window
 {
-	public MainWindow()
-	{
-		this.InitializeComponent();
-	}
-
-	private void myButton_Click(object sender, RoutedEventArgs e)
-	{
-		myButton.Content = "Clicked";
-	}
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
 }
