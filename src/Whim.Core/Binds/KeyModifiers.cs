5a2adeb14b6c574a8912d74af3f6d793e7e14901
dalyIsaac <isaac.daly@outlook.com>
2021-12-25 15:26:07 +1300

    Prevent the use of non-specific modifiers

--- src/Whim.Core/Binds/KeyModifiers.cs
+++ src/Whim.Core/Binds/KeyModifiers.cs
@@ -9,17 +9,13 @@ public enum KeyModifiers
 
 	LControl = 1,
 	RControl = 2,
-	Control = LControl | RControl,
 
 	LShift = 4,
 	RShift = 8,
-	Shift = LShift | RShift,
 
 	LAlt = 16,
 	RAlt = 32,
-	Alt = LAlt | RAlt,
 
 	LWin = 64,
 	RWin = 128,
-	Win = LWin | RWin
 }
