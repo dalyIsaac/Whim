4ca43f39f70696d82ff60bf050c9b347ec145076
dalyIsaac <isaac.daly@outlook.com>
2022-02-26 20:32:06 +1100

    Added Info logs to TreeLayoutEngine

--- src/Whim.TreeLayout/TreeLayoutEngine.cs
+++ src/Whim.TreeLayout/TreeLayoutEngine.cs
@@ -84,6 +84,8 @@ public partial class TreeLayoutEngine : ILayoutEngine
 
 		if (focusedWindow == null || !_windows.TryGetValue(focusedWindow, out LeafNode? focusedLeaf))
 		{
+			Logger.Information($"No focused window found. Looking for the right-most leaf node.");
+
 			// We can't find the focused window, so we'll just add it to the right-most node.
 			focusedLeaf = Root switch
 			{
@@ -105,6 +107,8 @@ public partial class TreeLayoutEngine : ILayoutEngine
 		// If the parent node is null, then the focused leaf is the root and we need to create a new split node.
 		if (focusedLeaf.Parent == null)
 		{
+			Logger.Information($"Focused leaf node {focusedLeaf.Window.Title} is the root node. Creating a new split node.");
+
 			// Create a new split node, and update the root.
 			SplitNode splitNode = new(FocusDirection);
 			splitNode.Add(focusedLeaf);
@@ -119,6 +123,8 @@ public partial class TreeLayoutEngine : ILayoutEngine
 		// add the window to the split node.
 		if (parent.Direction == FocusDirection)
 		{
+			Logger.Information($"Focused leaf node {focusedLeaf.Window.Title} is in a split node with direction {FocusDirection}. Adding window {window.Title} to the split node.");
+
 			parent.Add(newLeaf);
 			return newLeaf;
 		}
@@ -126,6 +132,8 @@ public partial class TreeLayoutEngine : ILayoutEngine
 		// If the parent node is a split node and the direction doesn't match, then we need to
 		// create a new split node and add the window to the split node.
 		// The focused leaf will also be added to the new split node.
+		Logger.Information($"Replacing the focused leaf node {focusedLeaf.Window.Title} with a split node with direction {FocusDirection}.");
+
 		SplitNode newSplitNode = new(FocusDirection, parent);
 		newSplitNode.Add(focusedLeaf);
 		newSplitNode.Add(newLeaf);
@@ -159,6 +167,8 @@ public partial class TreeLayoutEngine : ILayoutEngine
 		if (removingNode.Parent == null)
 		{
 			// The node is the root node.
+			Logger.Information($"The removed node was the root node");
+
 			Root = null;
 			return true;
 		}
@@ -169,17 +179,22 @@ public partial class TreeLayoutEngine : ILayoutEngine
 		// If the parent node has only one child, then we need to remove the split node.
 		if (parent.Count == 1)
 		{
+			Logger.Information($"The parent node had just one child. Removing the split node.");
+
 			(double _, Node child) = parent[0];
 			SplitNode? grandParent = parent.Parent;
 
 			if (grandParent == null)
 			{
 				// The parent is the root node.
+				Logger.Information($"The parent node was the root node");
+
 				Root = child;
 				return true;
 			}
 
 			// Since parent had just a single child, then replace parent with child.
+			Logger.Information("Replacing parent with child");
 			grandParent.Replace(parent, child);
 		}
 
