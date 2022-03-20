687874c0246ed4bd92e17840cdfd6e6954240b5d
dalyIsaac <isaac.daly@outlook.com>
2022-03-20 12:35:53 +1100

    Added tests for FocusWindowInDirection

---
+++ src/Whim.TreeLayout.Tests/TestFocusWindowInDirection.cs
@@ -0,0 +1,50 @@
+using Moq;
+using Xunit;
+
+namespace Whim.TreeLayout.Tests;
+
+public class TestFocusWindowInDirection
+{
+	[Fact]
+	public void IllegalWindow()
+	{
+		TestTreeEngine testTreeEngine = new();
+
+		// Try to focus a window that doesn't exist.
+		Mock<IWindow> illegalWindow = new();
+		testTreeEngine.Engine.FocusWindowInDirection(Direction.Left, illegalWindow.Object);
+
+		// Verify that the window was not focused.
+		illegalWindow.Verify(w => w.Focus(), Times.Never);
+	}
+
+	[Fact]
+	public void NoAdjacentWindow()
+	{
+		TestTreeEngine testTreeEngine = new();
+
+		// Set the currently focused window.
+		testTreeEngine.ActiveWorkspace.Setup(w => w.LastFocusedWindow).Returns(testTreeEngine.RightBottomWindow.Object);
+
+		// Try to focus to the left of the left window.
+		testTreeEngine.Engine.FocusWindowInDirection(Direction.Left, testTreeEngine.LeftWindow.Object);
+
+		// Verify that the window was not focused.
+		testTreeEngine.LeftWindow.Verify(w => w.Focus(), Times.Never);
+	}
+
+	[Fact]
+	public void FocusLeftWindow()
+	{
+		TestTreeEngine testTreeEngine = new();
+
+		// Set the currently focused window.
+		testTreeEngine.ActiveWorkspace.Setup(w => w.LastFocusedWindow).Returns(testTreeEngine.RightBottomWindow.Object);
+
+		// Try to focus to the left of the right window.
+		testTreeEngine.Engine.FocusWindowInDirection(Direction.Left, testTreeEngine.RightBottomWindow.Object);
+
+		// Verify that the window was focused.
+		testTreeEngine.LeftWindow.Verify(w => w.Focus(), Times.Once);
+	}
+}
