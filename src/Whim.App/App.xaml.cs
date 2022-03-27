7f621ffc5843deb2b6034a311a80078ed49d71be
dalyIsaac <isaac.daly@outlook.com>
2022-03-27 18:49:51 +1100

    Functional `Whim.App` running on WAS

--- src/Whim.App/App.xaml.cs
+++ src/Whim.App/App.xaml.cs
@@ -25,25 +25,28 @@ namespace Whim.App;
 /// </summary>
 public partial class App : Application
 {
-    /// <summary>
-    /// Initializes the singleton application object.  This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
-    /// </summary>
-    public App()
-    {
-        this.InitializeComponent();
-    }
+	private readonly IConfigContext _configContext;
 
-    /// <summary>
-    /// Invoked when the application is launched normally by the end user.  Other entry points
-    /// will be used such as when the application is launched to open a specific file.
-    /// </summary>
-    /// <param name="args">Details about the launch request and process.</param>
-    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
-    {
-        m_window = new MainWindow();
-        m_window.Activate();
-    }
+	public App(IConfigContext configContext)
+	{
+		Logger.Debug("Starting application...");
 
-    private Window m_window;
+		//Exit += Application_Exit;
+
+		InitializeComponent();
+
+		Logger.Debug("Initializing Whim");
+
+		_configContext = configContext;
+		_configContext.Initialize();
+	}
+
+	// Add when Windows App SDK supports the application exit event.
+	// https://docs.microsoft.com/en-us/windows/winui/api/microsoft.ui.xaml.application?view=winui-3.0
+	//private void Application_Exit(object sender, ExitEventArgs e)
+	//{
+	//	Logger.Information("Application exiting");
+	//	_configContext.Quit();
+	//	Logger.Information("Application disposed");
+	//}
 }
---
+++ src/Whim.App/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whim.App;
+
+#if DISABLE_XAML_GENERATED_MAIN
+public static class Program
+{
+	[global::System.Runtime.InteropServices.DllImport("Microsoft.ui.xaml.dll")]
+	private static extern void XamlCheckProcessRequirements();
+
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.UI.Xaml.Markup.Compiler", " 1.0.0.0")]
+	[global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+	[global::System.STAThreadAttribute]
+	// Replaces the standard App.g.i.cs.
+	// Note: We can't declare Main to be async because in a WinUI app
+	// that prevents Narrator from reading XAML elements.
+	static void Main(string[] args)
+	{
+		XamlCheckProcessRequirements();
+
+		global::WinRT.ComWrappersSupport.InitializeComWrappers();
+
+		bool this_is_the_first_instance = true;
+
+		// If this is the first instance launched, then register it as the "main" instance.
+		// If this isn't the first instance launched, then "main" will already be registered,
+		// so retrieve it.
+		var mainInstance = Microsoft.Windows.AppLifecycle.AppInstance.FindOrRegisterForKey("main");
+
+		// If the instance that's executing the OnLaunched handler right now
+		// isn't the "main" instance.
+		if (!mainInstance.IsCurrent)
+		{
+			this_is_the_first_instance = false;
+
+			// Redirect the activation (and args) to the "main" instance, and exit.
+			var activatedEventArgs =
+				Microsoft.Windows.AppLifecycle.AppInstance.GetCurrent().GetActivatedEventArgs();
+			mainInstance.RedirectActivationToAsync(activatedEventArgs);
+		}
+
+		if (this_is_the_first_instance)
+		{
+			global::Microsoft.UI.Xaml.Application.Start((p) =>
+			{
+				var context = new global::Microsoft.UI.Dispatching.DispatcherQueueSynchronizationContext(global::Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread());
+				global::System.Threading.SynchronizationContext.SetSynchronizationContext(context);
+				new App(Config.CreateConfigContext());
+			});
+		}
+	}
+}
+#endif
