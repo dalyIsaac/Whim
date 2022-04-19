72d1e81a119148501d34b4eed7a85d8520a45f52
dalyIsaac <isaac.daly@outlook.com>
2022-04-19 10:23:27 +1000

    Suppressed styl warnings for Whim.TreeLayout.Tests

---
+++ src/Whim.TreeLayout.Tests/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0008:Use explicit type")]
+[assembly: SuppressMessage("Style", "IDE0042:Variable declaration can be deconstructed")]
