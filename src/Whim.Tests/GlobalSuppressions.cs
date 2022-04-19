8504f39c086cc83f0704a44eb44a9469204fecd2
dalyIsaac <isaac.daly@outlook.com>
2022-04-19 16:21:50 +1000

    Added all analyzers for Whim.Tests

---
+++ src/Whim.Tests/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+// The general justification for these suppression messages is that this project contains tests, and it's
+// a bit excessive to have these particular rules for tests.
+[assembly: SuppressMessage("Design", "CA1014:Mark assemblies with CLSCompliantAttribute")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores")]
+[assembly: SuppressMessage("Security", "CA5394:Do not use insecure randomness")]
