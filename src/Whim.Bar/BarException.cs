7a4ee1f7eca670c9e6dbc592d693f86a30be3f11
dalyIsaac <isaac.daly@outlook.com>
2022-04-19 17:25:19 +1000

    Update exceptions to include more boilerplate

--- src/Whim.Bar/BarException.cs
+++ src/Whim.Bar/BarException.cs
@@ -2,17 +2,13 @@ using System;
 
 namespace Whim.Bar;
 
+[Serializable]
 public class BarException : Exception
 {
-	public BarException()
-	{
-	}
-
-	public BarException(string message) : base(message)
-	{
-	}
-
-	public BarException(string message, Exception innerException) : base(message, innerException)
-	{
-	}
+	public BarException() { }
+	public BarException(string message) : base(message) { }
+	public BarException(string message, Exception inner) : base(message, inner) { }
+	protected BarException(
+		System.Runtime.Serialization.SerializationInfo info,
+		System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 }
