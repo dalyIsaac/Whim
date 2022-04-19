4aec10ed9f4e0241462810b193ef82de3dee208a
dalyIsaac <isaac.daly@outlook.com>
2022-04-19 16:59:49 +1000

    Enabled analyzers in Whim.Gaps Deferred fixing some warnings until later

--- src/Whim.Gaps/GapsConfig.cs
+++ src/Whim.Gaps/GapsConfig.cs
@@ -5,12 +5,12 @@ public class GapsConfig
 	/// <summary>
 	/// The gap between the parent layout engine and the area where windows are placed.
 	/// </summary>
-	public int OuterGap { get; set; } = 0;
+	public int OuterGap { get; set; }
 
 	/// <summary>
 	/// The gap between windows.
 	/// </summary>
-	public int InnerGap { get; set; } = 0;
+	public int InnerGap { get; set; }
 
 	public GapsConfig(int outerGap = 0, int innerGap = 0)
 	{
