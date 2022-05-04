cfb5211135c861ff624f518e2b0636eed49a2f1f
dalyIsaac <isaac.daly@outlook.com>
2022-05-04 20:39:53 +1000

    ISS file now has relative paths

--- scripts/buildandinstall.ps1
+++ scripts/buildandinstall.ps1
@@ -1,5 +1,5 @@
 # Build the installer.
-& "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" src\whim-install.iss
+& "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" .\whim-installer.iss
 
 # Install Whim.
-.\src\whim-install.exe
+.\bin\whim-install.exe
