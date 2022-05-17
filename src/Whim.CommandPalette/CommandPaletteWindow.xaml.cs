1b6e39b9668e4722eeccf4e1663f99a22d551d3a
dalyIsaac <isaac.daly@outlook.com>
2022-05-17 19:18:20 +1000

    Fixed lists performance and selected item flickering

--- src/Whim.CommandPalette/CommandPaletteWindow.xaml.cs
+++ src/Whim.CommandPalette/CommandPaletteWindow.xaml.cs
@@ -1,29 +1,49 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Whim.CommandPalette;
 
 public sealed partial class CommandPaletteWindow : Microsoft.UI.Xaml.Window
 {
 	private readonly IConfigContext _configContext;
+	private readonly CommandPalettePlugin _plugin;
+
 	private readonly IWindow _window;
 	private IMonitor? _monitor;
 	public bool IsVisible => _monitor != null;
-	public CommandPaletteViewModel ViewModel { get; }
+
+	/// <summary>
+	/// The current commands from which <see cref="Matches"/> is derived.
+	/// </summary>
+	private readonly List<CommandPaletteMatch> _allCommands = new();
+
+	/// <summary>
+	/// The matching commands which are shown to the user.
+	/// </summary>
+	private readonly ObservableCollection<CommandPaletteMatch> _matches = new();
 
 	public CommandPaletteWindow(IConfigContext configContext, CommandPalettePlugin plugin)
 	{
 		_configContext = configContext;
+		_plugin = plugin;
 		_window = this.InitializeBorderlessWindow("Whim.CommandPalette", "CommandPaletteWindow", _configContext);
 
 		Title = CommandPaletteConfig.Title;
-		ViewModel = new CommandPaletteViewModel(configContext, plugin);
+		CommandListItems.ItemsSource = _matches;
 	}
 
 	public void Activate(IEnumerable<(ICommand, IKeybind?)>? items = null, IMonitor? monitor = null)
 	{
-		ViewModel.Activate(items);
-		monitor ??= _configContext.MonitorManager.FocusedMonitor;
+		foreach ((ICommand command, IKeybind? keybind) in items ?? _configContext.CommandManager)
+		{
+			if (command.CanExecute())
+			{
+				_allCommands.Add(new CommandPaletteMatch(command, keybind));
+			}
+		}
+		UpdateMatches();
 
+		monitor ??= _configContext.MonitorManager.FocusedMonitor;
 		if (monitor == _monitor)
 		{
 			return;
@@ -64,5 +84,66 @@ public sealed partial class CommandPaletteWindow : Microsoft.UI.Xaml.Window
 		{
 			Activate();
 		}
+	}
+
+	private void TextEntry_KeyDown(object _sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+	{
+		int selectedIndex = CommandListItems.SelectedIndex;
+		switch (e.Key)
+		{
+			case Windows.System.VirtualKey.Down:
+				CommandListItems.SelectedIndex = (selectedIndex + 1) % _matches.Count;
+				break;
+			case Windows.System.VirtualKey.Up:
+				CommandListItems.SelectedIndex = (selectedIndex + _matches.Count - 1) % _matches.Count;
+				break;
+			case Windows.System.VirtualKey.Enter:
+				// TODO: Execute
+				break;
+			case Windows.System.VirtualKey.Escape:
+				Hide();
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void TextEntry_TextChanged(object sender, Microsoft.UI.Xaml.Controls.TextChangedEventArgs e)
+	{
+		UpdateMatches();
+	}
+
+	private void UpdateMatches()
+	{
+		string query = TextEntry.Text;
+		int idx = 0;
+
+		IEnumerator<CommandPaletteMatch> enumerator = _plugin.Config.Matcher.Match(
+			query,
+			_allCommands,
+			_configContext,
+			_plugin
+		).GetEnumerator();
+
+		while (enumerator.MoveNext() && idx < _matches.Count)
+		{
+			if (_matches[idx] != enumerator.Current)
+			{
+				_matches[idx] = enumerator.Current;
+			}
+			idx++;
+		}
+
+		for (; idx < _matches.Count; idx++)
+		{
+			_matches.RemoveAt(idx);
+		}
+
+		while (enumerator.MoveNext())
+		{
+			_matches.Add(enumerator.Current);
+		}
+
+		CommandListItems.SelectedIndex = _matches.Count > 0 ? 0 : -1;
 	}
 }
