060799735cc723df289f02871889fece87e4fb20
dalyIsaac <isaac.daly@outlook.com>
2022-05-18 20:30:53 +1000

    Formatted key bindings

--- src/Whim.CommandPalette/CommandPaletteMatch.cs
+++ src/Whim.CommandPalette/CommandPaletteMatch.cs
@@ -1,13 +1,17 @@
+using System.Collections.ObjectModel;
+
 namespace Whim.CommandPalette;
 
 public class CommandPaletteMatch
 {
 	public ICommand Command { get; }
-	public string Keybind { get; }
+	public ReadOnlyCollection<string> Keybind { get; }
 
 	public CommandPaletteMatch(ICommand command, IKeybind? keybind)
 	{
 		Command = command;
-		Keybind = keybind?.ToString() ?? "";
+
+		string[] parts = (keybind?.ToString() ?? "").Split(" + ");
+		Keybind = new ReadOnlyCollection<string>(parts);
 	}
 }
