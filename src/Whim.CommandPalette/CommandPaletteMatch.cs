00d2ae9616772d793f4b6de66ca27e597606c483
dalyIsaac <isaac.daly@outlook.com>
2022-05-19 22:08:34 +1000

    Gave up on having separate keys

--- src/Whim.CommandPalette/CommandPaletteMatch.cs
+++ src/Whim.CommandPalette/CommandPaletteMatch.cs
@@ -1,17 +1,13 @@
-using System.Collections.ObjectModel;
-
 namespace Whim.CommandPalette;
 
 public class CommandPaletteMatch
 {
 	public ICommand Command { get; }
-	public ReadOnlyCollection<string> Keybind { get; }
+	public string? Keybind { get; }
 
 	public CommandPaletteMatch(ICommand command, IKeybind? keybind)
 	{
 		Command = command;
-
-		string[] parts = (keybind?.ToString() ?? "").Split(" + ");
-		Keybind = new ReadOnlyCollection<string>(parts);
+		Keybind = keybind?.ToString() ?? "";
 	}
 }
