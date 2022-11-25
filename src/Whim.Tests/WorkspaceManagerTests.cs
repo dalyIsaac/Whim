db444ac7c00f71f8fe77c5f12d5d334c868badff
Isaac Daly <isaac.daly@outlook.com>
2022-11-25 20:58:50 +1100

    Fix warnings

--- src/Whim.Tests/WorkspaceManagerTests.cs
+++ src/Whim.Tests/WorkspaceManagerTests.cs
@@ -61,7 +61,7 @@ public class WorkspaceManagerTests
 		Assert.Raises<MonitorWorkspaceChangedEventArgs>(
 			h => workspaceManager.MonitorWorkspaceChanged += h,
 			h => workspaceManager.MonitorWorkspaceChanged -= h,
-			() => workspaceManager.Initialize()
+			workspaceManager.Initialize
 		);
 
 		// The workspaces are initialized
