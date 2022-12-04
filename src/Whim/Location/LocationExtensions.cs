7b7e9389a7f16c7b37537a3e661e7a81e7deda63
Isaac Daly <isaac.daly@outlook.com>
2022-12-04 14:31:53 +1100

    Formatting

--- src/Whim/Location/LocationExtensions.cs
+++ src/Whim/Location/LocationExtensions.cs
@@ -14,5 +14,10 @@ public static class LocationExtensions
 	/// <param name="other">The location to add.</param>
 	/// <returns>A new <see cref="ILocation{T}"/> with the given <paramref name="other"/> added to this one.</returns>
 	public static ILocation<T> Add<T>(this ILocation<T> location, ILocation<T> other) where T : INumber<T> =>
-		new Location<T>(location.X + other.X, location.Y + other.Y, location.Width + other.Width, location.Height + other.Height);
+		new Location<T>(
+			location.X + other.X,
+			location.Y + other.Y,
+			location.Width + other.Width,
+			location.Height + other.Height
+		);
 }
