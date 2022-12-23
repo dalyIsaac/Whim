dd3bf3e0ffbb42786d7869eec6691a61bf168d17
Isaac Daly <isaac.daly@outlook.com>
2022-12-24 12:22:25 +1300

    Formatting

--- src/Whim/Monitor/MonitorEnumCallback.cs
+++ src/Whim/Monitor/MonitorEnumCallback.cs
@@ -5,12 +5,12 @@ using Windows.Win32.Graphics.Gdi;
 namespace Whim;
 
 internal class MonitorEnumCallback
-	{
-		public List<HMONITOR> Monitors { get; } = new();
+{
+	public List<HMONITOR> Monitors { get; } = new();
 
-		public unsafe BOOL Callback(HMONITOR monitor, HDC hdc, RECT* rect, LPARAM param)
-		{
-			Monitors.Add(monitor);
-			return (BOOL)true;
-		}
+	public unsafe BOOL Callback(HMONITOR monitor, HDC hdc, RECT* rect, LPARAM param)
+	{
+		Monitors.Add(monitor);
+		return (BOOL)true;
 	}
+}
