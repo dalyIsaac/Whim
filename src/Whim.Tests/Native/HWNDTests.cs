8f16b2b276d043a4df3fe48e9e0051ef5d868b23
Isaac Daly <isaac.daly@outlook.com>
2023-01-06 16:17:56 +1300

    Added Whim native tests

---
+++ src/Whim.Tests/Native/HWNDTests.cs
@@ -0,0 +1,51 @@
+using Windows.Win32.Foundation;
+using Xunit;
+
+namespace Whim.Tests;
+
+public class HWNDTests
+{
+	[Fact]
+	public void Null()
+	{
+		// Given
+		HWND hwnd = HWND.Null;
+
+		// When
+		bool result = hwnd.IsNull;
+
+		// Then
+		Assert.True(result);
+		Assert.Equal(0, hwnd.Value);
+	}
+
+	[Fact]
+	public void NotNull()
+	{
+		// Given
+		HWND hwnd = new(1);
+
+		// When
+		bool result = hwnd.IsNull;
+
+		// Then
+		Assert.False(result);
+		Assert.Equal(1, hwnd.Value);
+	}
+
+	[Theory]
+	[InlineData(1, 1, true)]
+	[InlineData(1, 2, false)]
+	public void Equals_Method(int value1, int value2, bool expected)
+	{
+		// Given
+		HWND hwnd1 = new(value1);
+		HWND hwnd2 = new(value2);
+
+		// When
+		bool result = hwnd1.Equals(hwnd2);
+
+		// Then
+		Assert.Equal(expected, result);
+	}
+}
