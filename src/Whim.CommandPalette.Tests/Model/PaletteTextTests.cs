a47b84c9c1d4749cca6c1aa1bbf61df38bab68c7
Isaac Daly <isaac.daly@outlook.com>
2023-02-12 14:36:58 +1100

    Added MatcherResult test

--- src/Whim.CommandPalette.Tests/Model/PaletteTextTests.cs
+++ src/Whim.CommandPalette.Tests/Model/PaletteTextTests.cs
@@ -5,21 +5,6 @@ namespace Whim.CommandPalette.Tests;
 public class PaletteTextTests
 {
 	[Fact]
-	public void ImplicitOperator()
-	{
-		// Given
-		string text = "Implicit";
-
-		// When
-		PaletteText paletteText = new(text);
-
-		// Then
-		Assert.Single(paletteText.Segments);
-		Assert.Equal("Implicit", paletteText.Segments[0].Text);
-		Assert.False(paletteText.Segments[0].IsHighlighted);
-	}
-
-	[Fact]
 	public void FromString()
 	{
 		// Given
