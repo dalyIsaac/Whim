3fdb722a51539715142d5c28abb640456330b8af
Isaac Daly <isaac.daly@outlook.com>
2023-02-28 21:27:39 +1100

    Moved RegexTests and SubstringTests

--- src/Whim.CommandPalette.WinUITests/Filters/SubstringTests.cs
+++ src/Whim.CommandPalette.WinUITests/Filters/SubstringTests.cs
@@ -1,35 +1,36 @@
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Whim.CommandPalette.Tests;
+namespace Whim.CommandPalette.WinUITests;
 
+[TestClass]
 public class SubstringTests
 {
-	[InlineData("cela", "cancelAnimationFrame()", new int[] { 3, 7 })]
-	[Theory]
+	[TestMethod]
+	[DataRow("cela", "cancelAnimationFrame()", new int[] { 3, 7 })]
 	public void MatchesContiguousSubString_Ok(string word, string wordToMatchAgainst, params int[][] expected)
 	{
 		FilterTextMatch[] expectedMatches = FilterTestUtils.CreateExpectedMatches(expected);
 		FilterTestUtils.FilterOk(PaletteFilters.MatchesContiguousSubString, word, wordToMatchAgainst, expectedMatches);
 	}
 
-	[Fact]
+	[TestMethod]
 	public void MatchesContiguousSubString_NotFound()
 	{
 		FilterTestUtils.FilterNotOk(PaletteFilters.MatchesContiguousSubString, "cmm", "cancelAnimationFrame()");
 	}
 
-	[InlineData("cmm", "cancelAnimationFrame()", new int[] { 0, 1 }, new int[] { 9, 10 }, new int[] { 18, 19 })]
-	[InlineData("abc", "abcabc", new int[] { 0, 3 })]
-	[InlineData("abc", "aaabbbccc", new int[] { 0, 1 }, new int[] { 3, 4 }, new int[] { 6, 7 })]
-	[Theory]
+	[TestMethod]
+	[DataRow("cmm", "cancelAnimationFrame()", new int[] { 0, 1 }, new int[] { 9, 10 }, new int[] { 18, 19 })]
+	[DataRow("abc", "abcabc", new int[] { 0, 3 })]
+	[DataRow("abc", "aaabbbccc", new int[] { 0, 1 }, new int[] { 3, 4 }, new int[] { 6, 7 })]
 	public void MatchesSubString_Ok(string word, string wordToMatchAgainst, params int[][] expected)
 	{
 		FilterTextMatch[] expectedMatches = FilterTestUtils.CreateExpectedMatches(expected);
 		FilterTestUtils.FilterOk(PaletteFilters.MatchesSubString, word, wordToMatchAgainst, expectedMatches);
 	}
 
-	[InlineData("aaaaaaaaaaaaaaaaaaaax", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")]
-	[Theory]
+	[TestMethod]
+	[DataRow("aaaaaaaaaaaaaaaaaaaax", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")]
 	public void MatchesSubString_NotOk(string word, string wordToMatchAgainst)
 	{
 		FilterTestUtils.FilterNotOk(PaletteFilters.MatchesSubString, word, wordToMatchAgainst);
