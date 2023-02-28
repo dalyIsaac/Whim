3923334e103323de275f964bb3634e01111ed554
Isaac Daly <isaac.daly@outlook.com>
2023-02-28 21:24:55 +1100

    Moved PrefixTests

--- src/Whim.CommandPalette.WinUITests/Filters/PrefixTests.cs
+++ src/Whim.CommandPalette.WinUITests/Filters/PrefixTests.cs
@@ -1,48 +1,49 @@
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Whim.CommandPalette.Tests;
+namespace Whim.CommandPalette.WinUITests;
 
+[TestClass]
 public class PrefixTests
 {
-	[InlineData("", "anything")]
-	[InlineData("alpha", "alpha", new int[] { 0, 5 })]
-	[InlineData("alpha", "alphasomething", new int[] { 0, 5 })]
-	[InlineData("a", "alpha", new int[] { 0, 1 })]
-	[Theory]
+	[TestMethod]
+	[DataRow("", "anything")]
+	[DataRow("alpha", "alpha", new int[] { 0, 5 })]
+	[DataRow("alpha", "alphasomething", new int[] { 0, 5 })]
+	[DataRow("a", "alpha", new int[] { 0, 1 })]
 	public void MatchesStrictPrefix_Ok(string word, string wordToMatchAgainst, params int[][] expected)
 	{
 		FilterTextMatch[] expectedMatches = FilterTestUtils.CreateExpectedMatches(expected);
 		FilterTestUtils.FilterOk(PaletteFilters.MatchesStrictPrefix, word, wordToMatchAgainst, expectedMatches);
 	}
 
-	[InlineData("", "")]
-	[InlineData("alpha", "alp")]
-	[InlineData("x", "alpha")]
-	[InlineData("A", "alpha")]
-	[InlineData("AlPh", "alPHA")]
-	[Theory]
+	[TestMethod]
+	[DataRow("", "")]
+	[DataRow("alpha", "alp")]
+	[DataRow("x", "alpha")]
+	[DataRow("A", "alpha")]
+	[DataRow("AlPh", "alPHA")]
 	public void MatchesStrictPrefix_NotOk(string word, string wordToMatchAgainst)
 	{
 		FilterTestUtils.FilterNotOk(PaletteFilters.MatchesStrictPrefix, word, wordToMatchAgainst);
 	}
 
-	[InlineData("alpha", "alpha", new int[] { 0, 5 })]
-	[InlineData("alpha", "alphasomething", new int[] { 0, 5 })]
-	[InlineData("a", "alpha", new int[] { 0, 1 })]
-	[InlineData("ä", "Älpha", new int[] { 0, 1 })]
-	[InlineData("A", "alpha", new int[] { 0, 1 })]
-	[InlineData("AlPh", "alPHA", new int[] { 0, 4 })]
-	[Theory]
+	[TestMethod]
+	[DataRow("alpha", "alpha", new int[] { 0, 5 })]
+	[DataRow("alpha", "alphasomething", new int[] { 0, 5 })]
+	[DataRow("a", "alpha", new int[] { 0, 1 })]
+	[DataRow("ä", "Älpha", new int[] { 0, 1 })]
+	[DataRow("A", "alpha", new int[] { 0, 1 })]
+	[DataRow("AlPh", "alPHA", new int[] { 0, 4 })]
 	public void MatchesPrefix_Ok(string word, string wordToMatchAgainst, params int[][] expected)
 	{
 		FilterTextMatch[] expectedMatches = FilterTestUtils.CreateExpectedMatches(expected);
 		FilterTestUtils.FilterOk(PaletteFilters.MatchesPrefix, word, wordToMatchAgainst, expectedMatches);
 	}
 
-	[InlineData("alpha", "alp")]
-	[InlineData("x", "alpha")]
-	[InlineData("T", "4")]
-	[Theory]
+	[TestMethod]
+	[DataRow("alpha", "alp")]
+	[DataRow("x", "alpha")]
+	[DataRow("T", "4")]
 	public void MatchesPrefix_NotOk(string word, string wordToMatchAgainst)
 	{
 		FilterTestUtils.FilterNotOk(PaletteFilters.MatchesPrefix, word, wordToMatchAgainst);
