51b5ccb325b0d1234ede5b4c108b7616a2a696aa
Isaac Daly <isaac.daly@outlook.com>
2023-02-28 20:48:03 +1100

    Added PaletteTextSegment tests

---
+++ src/Whim.CommandPalette.WinUITests/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace Whim.CommandPalette.WinUITests;
+
+public sealed partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Whim.CommandPalette.WinUITests/Model/PaletteTextTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.UI.Text;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+
+namespace Whim.CommandPalette.WinUITests.Model;
+
+[TestClass]
+public class PaletteTextSegmentTests
+{
+	[UITestMethod]
+	public void PaletteTextSegment_ToRun()
+	{
+		// Arrange
+		PaletteTextSegment segment = new("Test", true);
+
+		// Act
+		Run run = segment.ToRun();
+
+		// Assert
+		Assert.AreEqual("Test", run.Text);
+		Assert.AreEqual(FontWeights.Bold, run.FontWeight);
+	}
+
+	[UITestMethod]
+	public void PaletteTextSegment_ToRun_NoHighlight()
+	{
+		// Arrange
+		PaletteTextSegment segment = new("Test", false);
+
+		// Act
+		Run run = segment.ToRun();
+
+		// Assert
+		Assert.AreEqual("Test", run.Text);
+		Assert.AreEqual(FontWeights.Normal, run.FontWeight);
+	}
+}
