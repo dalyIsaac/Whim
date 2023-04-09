affa37e4c02f3803168680f7ddfb502b6e927986
Isaac Daly <isaac.daly@outlook.com>
2023-04-09 16:42:39 +1000

    Add close button to PhantomWindow

--- src/Whim.TreeLayout/PhantomWindow.xaml.cs
+++ src/Whim.TreeLayout/PhantomWindow.xaml.cs
@@ -8,4 +8,9 @@ internal sealed partial class PhantomWindow : Microsoft.UI.Xaml.Window
 		this.InitializeBorderlessWindow(configContext, "Whim.TreeLayout", "PhantomWindow");
 		this.SetIsShownInSwitchers(false);
 	}
+
+	private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+	{
+		this.Close();
+	}
 }
