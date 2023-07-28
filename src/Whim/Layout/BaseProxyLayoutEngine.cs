c41de5df2477c1739335d96605d10e2320b60075
Isaac Daly <isaac.daly@outlook.com>
2023-07-28 21:27:21 +1000

    BaseProxyLayoutEngine no longer implements most methods. This forces proxies to explicitly handle functionality.

--- src/Whim/Layout/BaseProxyLayoutEngine.cs
+++ src/Whim/Layout/BaseProxyLayoutEngine.cs
@@ -19,17 +19,17 @@ public abstract class BaseProxyLayoutEngine : ILayoutEngine
 	/// </summary>
 	protected ILayoutEngine InnerLayoutEngine { get; }
 
-	/// <inheritdoc/>
-	public LayoutEngineIdentity Identity => InnerLayoutEngine.Identity;
-
 	/// <summary>
-	/// Constructs a new proxy layout engine.
+	/// Creates a new <cref name="BaseProxyLayoutEngine"/> with the given <paramref name="innerLayoutEngine"/>.
 	/// </summary>
-	/// <param name="innerLayoutEngine">The proxied layout engine.</param>
+	/// <param name="innerLayoutEngine"></param>
 	protected BaseProxyLayoutEngine(ILayoutEngine innerLayoutEngine)
 	{
 		InnerLayoutEngine = innerLayoutEngine;
 	}
+
+	/// <inheritdoc/>
+	public LayoutEngineIdentity Identity => InnerLayoutEngine.Identity;
 
 	/// <summary>
 	/// Updates the layout engine with the new proxied layout engine.
@@ -52,44 +52,40 @@ public abstract class BaseProxyLayoutEngine : ILayoutEngine
 	/// The name is only really important for the user, so we can use the name of the proxied layout engine.
 	/// </summary>
 	/// <inheritdoc/>
-	public virtual string Name => InnerLayoutEngine.Name;
+	public string Name => InnerLayoutEngine.Name;
 
 	/// <inheritdoc/>
-	public virtual int Count => InnerLayoutEngine.Count;
+	public abstract int Count { get; }
 
 	/// <inheritdoc/>
-	public virtual ILayoutEngine AddWindow(IWindow window) => Update(InnerLayoutEngine.AddWindow(window));
+	public abstract ILayoutEngine AddWindow(IWindow window);
 
 	/// <inheritdoc/>
-	public virtual ILayoutEngine MoveWindowToPoint(IWindow window, IPoint<double> point) =>
-		Update(InnerLayoutEngine.MoveWindowToPoint(window, point));
+	public abstract ILayoutEngine MoveWindowToPoint(IWindow window, IPoint<double> point);
 
 	/// <inheritdoc/>
-	public virtual ILayoutEngine RemoveWindow(IWindow window) => Update(InnerLayoutEngine.RemoveWindow(window));
+	public abstract ILayoutEngine RemoveWindow(IWindow window);
 
 	/// <inheritdoc/>
-	public virtual IWindow? GetFirstWindow() => InnerLayoutEngine.GetFirstWindow();
+	public abstract IWindow? GetFirstWindow();
 
 	/// <inheritdoc/>
-	public virtual void FocusWindowInDirection(Direction direction, IWindow window) =>
-		InnerLayoutEngine.FocusWindowInDirection(direction, window);
+	public abstract void FocusWindowInDirection(Direction direction, IWindow window);
 
 	/// <inheritdoc/>
-	public virtual ILayoutEngine SwapWindowInDirection(Direction direction, IWindow window) =>
-		Update(InnerLayoutEngine.SwapWindowInDirection(direction, window));
+	public abstract ILayoutEngine SwapWindowInDirection(Direction direction, IWindow window);
 
 	/// <inheritdoc/>
-	public virtual bool ContainsWindow(IWindow window) => InnerLayoutEngine.ContainsWindow(window);
+	public abstract bool ContainsWindow(IWindow window);
 
 	/// <inheritdoc/>
-	public virtual ILayoutEngine MoveWindowEdgesInDirection(Direction edge, IPoint<double> deltas, IWindow window) =>
-		Update(InnerLayoutEngine.MoveWindowEdgesInDirection(edge, deltas, window));
+	public abstract ILayoutEngine MoveWindowEdgesInDirection(Direction edge, IPoint<double> deltas, IWindow window);
 
 	/// <inheritdoc/>
 	public abstract IEnumerable<IWindowState> DoLayout(ILocation<int> location, IMonitor monitor);
 
 	/// <inheritdoc/>
-	public virtual void HidePhantomWindows() => InnerLayoutEngine.HidePhantomWindows();
+	public abstract void HidePhantomWindows();
 
 	/// <summary>
 	/// Checks to see if this <cref name="IImmutableLayoutEngine"/>
