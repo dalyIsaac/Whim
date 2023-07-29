f0806fc648c893a2f9b0f86927ac713498a2efda
Isaac Daly <isaac.daly@outlook.com>
2023-07-29 22:23:37 +1000

    FloatingLayoutPlugin coverage

--- src/Whim.FloatingLayout.Tests/FloatingLayoutPluginTests.cs
+++ src/Whim.FloatingLayout.Tests/FloatingLayoutPluginTests.cs
@@ -113,6 +113,37 @@ public class FloatingLayoutPluginTests
 		wrapper.WorkspaceManager.Verify(x => x.AddProxyLayoutEngine(It.IsAny<CreateProxyLayoutEngine>()), Times.Never);
 	}
 
+	[Fact]
+	public void WindowManager_WindowRemoved()
+	{
+		// Given
+		Mock<IWindow> window = new();
+		Mock<IMonitor> monitor = new();
+		Location<int> location = new();
+		WindowState windowState =
+			new()
+			{
+				Window = window.Object,
+				Location = location,
+				WindowSize = WindowSize.Normal
+			};
+
+		Wrapper wrapper = new();
+		wrapper
+			.Setup_GetWorkspaceForWindow(window.Object, wrapper.Workspace.Object)
+			.Setup_TryGetWindowLocation(window.Object, windowState)
+			.Setup_GetMonitorAtPoint(location, monitor);
+		FloatingLayoutPlugin plugin = wrapper.Plugin;
+
+		// When
+		plugin.PreInitialize();
+		plugin.MarkWindowAsFloating(window.Object);
+		wrapper.WindowManager.Raise(x => x.WindowRemoved += null, new WindowEventArgs() { Window = window.Object });
+
+		// Then nothing
+		Assert.Empty(plugin.FloatingWindows);
+	}
+
 	#region MarkWindowAsFloating
 	[Fact]
 	public void MarkWindowAsFloating_NoWindow()
@@ -213,8 +244,25 @@ public class FloatingLayoutPluginTests
 	}
 	#endregion
 
+	#region MarkWindowAsDocked
 	[Fact]
-	public void MarkWindowAsDocked()
+	public void MarkWindowAsDocked_WindowIsNotFloating()
+	{
+		// Given
+		Mock<IWindow> window = new();
+		Wrapper wrapper = new();
+		FloatingLayoutPlugin plugin = wrapper.Plugin;
+
+		// When
+		plugin.MarkWindowAsDocked(window.Object);
+
+		// Then
+		Assert.Empty(plugin.FloatingWindows);
+		wrapper.Workspace.Verify(w => w.MoveWindowToPoint(window.Object, It.IsAny<IPoint<double>>()), Times.Never);
+	}
+
+	[Fact]
+	public void MarkWindowAsDocked_WindowIsFloating()
 	{
 		// Given
 		Mock<IWindow> window = new();
@@ -244,6 +292,7 @@ public class FloatingLayoutPluginTests
 		Assert.Empty(plugin.FloatingWindows);
 		wrapper.Workspace.Verify(w => w.MoveWindowToPoint(window.Object, It.IsAny<IPoint<double>>()), Times.Exactly(2));
 	}
+	#endregion
 
 	#region ToggleWindowFloating
 	[Fact]
