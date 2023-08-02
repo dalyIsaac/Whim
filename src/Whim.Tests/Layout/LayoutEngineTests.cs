bdcb0b3945f844300c2191e0f0324349b4437c50
Isaac Daly <isaac.daly@outlook.com>
2023-08-02 21:42:49 +1000

    Remove duplicate test layout engine class

--- src/Whim.Tests/Layout/LayoutEngineTests.cs
+++ src/Whim.Tests/Layout/LayoutEngineTests.cs
@@ -7,39 +7,6 @@ namespace Whim.Tests;
 
 public class LayoutEngineTests
 {
-	private class TestLayoutEngine : ILayoutEngine
-	{
-		public LayoutEngineIdentity Identity => throw new System.NotImplementedException();
-		public string Name => throw new System.NotImplementedException();
-
-		public int Count => throw new System.NotImplementedException();
-
-		public ILayoutEngine AddWindow(IWindow window) => throw new System.NotImplementedException();
-
-		public ILayoutEngine MoveWindowToPoint(IWindow window, IPoint<double> point) =>
-			throw new System.NotImplementedException();
-
-		public bool ContainsWindow(IWindow window) => throw new System.NotImplementedException();
-
-		public IEnumerable<IWindowState> DoLayout(ILocation<int> location, IMonitor monitor) =>
-			throw new System.NotImplementedException();
-
-		public void FocusWindowInDirection(Direction direction, IWindow window) =>
-			throw new System.NotImplementedException();
-
-		public IWindow? GetFirstWindow() => throw new System.NotImplementedException();
-
-		public ILayoutEngine MoveWindowEdgesInDirection(Direction edges, IPoint<double> deltas, IWindow window) =>
-			throw new System.NotImplementedException();
-
-		public ILayoutEngine RemoveWindow(IWindow window) => throw new System.NotImplementedException();
-
-		public ILayoutEngine SwapWindowInDirection(Direction direction, IWindow window) =>
-			throw new System.NotImplementedException();
-	}
-
-	internal interface ITestLayoutEngine : ILayoutEngine { }
-
 	#region GetLayoutEngine
 	[Fact]
 	public void GetLayoutEngine_IsT()
