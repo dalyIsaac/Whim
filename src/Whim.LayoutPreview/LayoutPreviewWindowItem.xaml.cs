d8cdaeaf9069f78b27d9ba77b563334e7f2b7f12
Isaac Daly <isaac.daly@outlook.com>
2023-08-25 22:41:50 +1000

    Move GetTextColor

--- src/Whim.LayoutPreview/LayoutPreviewWindowItem.xaml.cs
+++ src/Whim.LayoutPreview/LayoutPreviewWindowItem.xaml.cs
@@ -1,7 +1,6 @@
 using Microsoft.UI;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
-using System;
 using Windows.UI;
 using Windows.UI.ViewManagement;
 
@@ -33,7 +32,7 @@ public sealed partial class LayoutPreviewWindowItem : UserControl
 		if (isHovered)
 		{
 			tintColor = GetHoverTintColor();
-			Title.Foreground = new SolidColorBrush(GetTextColor(tintColor));
+			Title.Foreground = new SolidColorBrush(tintColor.GetTextColor());
 		}
 		else
 		{
@@ -54,33 +53,5 @@ public sealed partial class LayoutPreviewWindowItem : UserControl
 	{
 		Color accentColor = new UISettings().GetColorValue(UIColorType.Accent);
 		return Color.FromArgb(255, accentColor.R, accentColor.G, accentColor.B);
-	}
-
-	// TODO: move to Whim's utils
-	/// <summary>
-	/// Gets the text color based on the given background color.
-	/// </summary>
-	/// <param name="backgroundColor"></param>
-	/// <returns></returns>
-	private static Color GetTextColor(Color backgroundColor)
-	{
-		double[] uiColors = new double[] { backgroundColor.R / 255, backgroundColor.G / 255, backgroundColor.B / 255 };
-
-		double[] cColors = new double[3];
-		for (int idx = 0; idx < 3; idx++)
-		{
-			double col = uiColors[idx];
-			if (col <= 0.03928)
-			{
-				cColors[idx] = col / 12.92;
-			}
-			else
-			{
-				cColors[idx] = Math.Pow((col + 0.055) / 1.055, 2.4);
-			}
-		}
-
-		double L = (0.2126 * cColors[0]) + (0.7152 * cColors[1]) + (0.0722 * cColors[2]);
-		return L > 0.179 ? Colors.White : Colors.Black;
 	}
 }
