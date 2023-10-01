e69e787e9c9ebe0a5b22d48729d90ce4b592579a
Isaac Daly <isaac.daly@outlook.com>
2023-10-01 12:23:02 +1100

    Increased NonNegativeValueConverter coverage

--- src/Whim.LayoutPreview.Tests/NonNegativeValueConverterTests.cs
+++ src/Whim.LayoutPreview.Tests/NonNegativeValueConverterTests.cs
@@ -8,13 +8,29 @@ public class NonNegativeValueConverterTests
 	[InlineData(0, 0)]
 	[InlineData(1, 1)]
 	[Theory]
-	public void Convert(int input, int expected)
+	public void Convert_Int(int input, int expected)
 	{
 		// Given
 		NonNegativeValueConverter converter = new();
 
 		// When
 		int actual = (int)converter.Convert(input, typeof(int), new object(), "")!;
+
+		// Then
+		Assert.Equal(expected, actual);
+	}
+
+	[InlineData(-1.0, 0)]
+	[InlineData(0.0, 0)]
+	[InlineData(1.0, 1)]
+	[Theory]
+	public void Convert_Double(double input, double expected)
+	{
+		// Given
+		NonNegativeValueConverter converter = new();
+
+		// When
+		double actual = (double)converter.Convert(input, typeof(double), new object(), "")!;
 
 		// Then
 		Assert.Equal(expected, actual);
