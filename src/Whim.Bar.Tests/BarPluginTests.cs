2c179645663e78b53c24a02fa5524fda1d496fdb
Isaac Daly <isaac.daly@outlook.com>
2023-10-08 15:56:18 +1100

    Increase coverage

---
+++ src/Whim.Bar.Tests/BarPluginTests.cs
@@ -0,0 +1,29 @@
+using NSubstitute;
+using Whim.TestUtils;
+using Xunit;
+
+namespace Whim.Bar.Tests;
+
+public class BarPluginTests
+{
+	[Theory, AutoSubstituteData]
+	public void MonitorManager_MonitorsChanged_RemovedMonitors(IContext context, IMonitor monitor)
+	{
+		// Given
+		BarConfig barConfig = new(new List<BarComponent>(), new List<BarComponent>(), new List<BarComponent>());
+		BarPlugin barPlugin = new(context, barConfig);
+
+		// When MonitorManager_MonitorsChanged is called with a removed monitor which is not in the map
+		context.MonitorManager.MonitorsChanged += Raise.EventWith(
+			new MonitorsChangedEventArgs()
+			{
+				AddedMonitors = Array.Empty<IMonitor>(),
+				UnchangedMonitors = Array.Empty<IMonitor>(),
+				RemovedMonitors = new[] { monitor }
+			}
+		);
+
+		// Then an exception is not thrown.
+		barPlugin.Dispose();
+	}
+}
