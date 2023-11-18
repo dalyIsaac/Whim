31f27b9bdfaac5c5a518452a884df671b3c2345a
Isaac Daly <isaac.daly@outlook.com>
2023-11-18 17:26:21 +1100

    Tests

--- src/Whim.Tests/Native/HMONITORTests.cs
+++ src/Whim.Tests/Native/HMONITORTests.cs
@@ -1,49 +1,21 @@
-using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Gdi;
 using Xunit;
 
 namespace Whim.Tests;
 
-public class HWNDTests
+public class HMONITORTests
 {
-	[Fact]
-	public void Null()
-	{
-		// Given
-		HWND hwnd = HWND.Null;
-
-		// When
-		bool result = hwnd.IsNull;
-
-		// Then
-		Assert.True(result);
-		Assert.Equal(0, hwnd.Value);
-	}
-
-	[Fact]
-	public void NotNull()
-	{
-		// Given
-		HWND hwnd = new(1);
-
-		// When
-		bool result = hwnd.IsNull;
-
-		// Then
-		Assert.False(result);
-		Assert.Equal(1, hwnd.Value);
-	}
-
 	[Theory]
 	[InlineData(1, 1, true)]
 	[InlineData(1, 2, false)]
 	public void Equals_Method(int value1, int value2, bool expected)
 	{
 		// Given
-		HWND hwnd1 = new(value1);
-		HWND hwnd2 = new(value2);
+		HMONITOR hmonitor1 = new(value1);
+		HMONITOR hmonitor2 = new(value2);
 
 		// When
-		bool result = hwnd1.Equals(hwnd2);
+		bool result = hmonitor1.Equals(hmonitor2);
 
 		// Then
 		Assert.Equal(expected, result);
@@ -55,11 +27,11 @@ public class HWNDTests
 	public void Equals_Operator(int value1, int value2, bool expected)
 	{
 		// Given
-		HWND hwnd1 = new(value1);
-		HWND hwnd2 = new(value2);
+		HMONITOR hmonitor1 = new(value1);
+		HMONITOR hmonitor2 = new(value2);
 
 		// When
-		bool result = hwnd1 == hwnd2;
+		bool result = hmonitor1 == hmonitor2;
 
 		// Then
 		Assert.Equal(expected, result);
@@ -71,11 +43,11 @@ public class HWNDTests
 	public void NotEquals_Operator(int value1, int value2, bool expected)
 	{
 		// Given
-		HWND hwnd1 = new(value1);
-		HWND hwnd2 = new(value2);
+		HMONITOR hmonitor1 = new(value1);
+		HMONITOR hmonitor2 = new(value2);
 
 		// When
-		bool result = hwnd1 != hwnd2;
+		bool result = hmonitor1 != hmonitor2;
 
 		// Then
 		Assert.Equal(expected, result);
@@ -85,10 +57,10 @@ public class HWNDTests
 	public void ToString_Hex()
 	{
 		// Given
-		HWND hwnd = new(0x12345);
+		HMONITOR hmonitor = new(0x12345);
 
 		// When
-		string result = hwnd.ToString();
+		string result = hmonitor.ToString();
 
 		// Then
 		Assert.Equal("0x00012345", result);
