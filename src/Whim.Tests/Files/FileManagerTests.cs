1621f88d6cfa3b5dd0264d3155ff32acd9bb65fd
Isaac Daly <isaac.daly@outlook.com>
2023-11-25 21:36:11 +1100

    Coverage

--- src/Whim.Tests/Files/FileManagerTests.cs
+++ src/Whim.Tests/Files/FileManagerTests.cs
@@ -6,8 +6,15 @@ namespace Whim.Tests;
 
 public class FileManagerTests
 {
-	private static readonly string ExpectedWhimDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".whim");
-	private static readonly string ExpectedAltWhimDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "test");
+	private static readonly string ExpectedWhimDir = Path.Combine(
+		Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+		".whim"
+	);
+	private static readonly string ExpectedAltWhimDir = Path.Combine(
+		Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+		"test"
+	);
+	private const string DIR_ARG = "--dir";
 
 	[Fact]
 	public void WhimDir()
@@ -26,8 +33,7 @@ public class FileManagerTests
 	public void WhimDir_WithDirArg()
 	{
 		// Given
-		string dirArg = "--dir";
-		string[] args = { dirArg, ExpectedAltWhimDir };
+		string[] args = { DIR_ARG, ExpectedAltWhimDir };
 		IFileManager fileManager = new FileManager(args);
 
 		// When
@@ -41,8 +47,7 @@ public class FileManagerTests
 	public void WhimDir_WithDirArg_WithExtraArg()
 	{
 		// Given
-		string dirArg = "--dir";
-		string[] args = { dirArg, ExpectedAltWhimDir, "--extra" };
+		string[] args = { DIR_ARG, ExpectedAltWhimDir, "--extra" };
 		IFileManager fileManager = new FileManager(args);
 
 		// When
@@ -53,11 +58,52 @@ public class FileManagerTests
 	}
 
 	[Fact]
+	public void WhimDir_WithDirArg_StartsWithExtraArg()
+	{
+		// Given
+		string[] args = { "--extra", DIR_ARG, ExpectedAltWhimDir };
+		IFileManager fileManager = new FileManager(args);
+
+		// When
+		string whimDirFromArgs = fileManager.WhimDir;
+
+		// Then
+		Assert.Equal(ExpectedAltWhimDir, whimDirFromArgs);
+	}
+
+	[Fact]
+	public void WhimDir_WithDirArg_NextIsNotArg()
+	{
+		// Given
+		string[] args = { DIR_ARG, "--extra", ExpectedAltWhimDir };
+		IFileManager fileManager = new FileManager(args);
+
+		// When
+		string whimDirFromArgs = fileManager.WhimDir;
+
+		// Then
+		Assert.Equal(ExpectedWhimDir, whimDirFromArgs);
+	}
+
+	[Fact]
+	public void WhimDir_WithDirArg_EmptyString()
+	{
+		// Given
+		string[] args = { DIR_ARG, string.Empty };
+		IFileManager fileManager = new FileManager(args);
+
+		// When
+		string whimDirFromArgs = fileManager.WhimDir;
+
+		// Then
+		Assert.Equal(ExpectedWhimDir, whimDirFromArgs);
+	}
+
+	[Fact]
 	public void WhimDir_WithDirArg_Equals()
 	{
 		// Given
-		string dirArg = "--dir";
-		string[] args = { dirArg + "=" + ExpectedAltWhimDir };
+		string[] args = { DIR_ARG + "=" + ExpectedAltWhimDir };
 		IFileManager fileManager = new FileManager(args);
 
 		// When
