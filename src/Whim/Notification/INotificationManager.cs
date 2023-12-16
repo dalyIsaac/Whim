150a91e8fc8e4ca18663cfa15512f113c420f4ef
Isaac Daly <isaac.daly@outlook.com>
2023-12-17 06:42:12 +1100

    Add `INotificationManager` to facilitate toast notifications (#721)
    Added `INotificationManager` to facilitate toast notifications. The core and plugins can display notifications and register listeners to handle user interaction with the notifications. This is a bit of a tricky API, as _all_ interactions need to set the `WHIM_NOTIFICATION_ID` argument in order for Whim to route the interaction to the correct listener.
    
    This will be used by the `Whim.Updater` in #141.

---
+++ src/Whim/Notification/INotificationManager.cs
@@ -0,0 +1,54 @@
+using System;
+using Microsoft.Windows.AppNotifications;
+
+namespace Whim;
+
+/// <summary>
+/// The notification manager allows the sending of toast notifications to the user.
+/// </summary>
+public interface INotificationManager : IDisposable
+{
+	/// <summary>
+	/// The key used to store the notification id in the notification arguments.
+	/// </summary>
+	public const string NotificationIdKey = "WHIM_NOTIFICATION_ID";
+
+	/// <summary>
+	/// Initialize the notification manager.
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// Register a notification handler for a given notification id.
+	/// </summary>
+	/// <param name="notificationId">
+	/// The notification id to register the handler for. <br/>
+	/// The notificationid should be prefixed by the plugin name to prevent collisions.
+	/// </param>
+	/// <param name="notificationReceived"></param>
+	void Register(string notificationId, Action<AppNotificationActivatedEventArgs> notificationReceived);
+
+	/// <summary>
+	/// Unregister a notification handler for a given notification id.
+	/// </summary>
+	/// <param name="notificationId"></param>
+	void Unregister(string notificationId);
+
+	/// <summary>
+	/// Send a toast notification to the user.
+	/// </summary>
+	/// <param name="appNotification">
+	/// The notification to send. <br/>
+	/// Make sure to call <c>SetArgument(INotificationManager.NOTIFICATION_ID_KEY, notificationId)</c>
+	/// to the notification and each component which can be interacted with. <br/>
+	/// Otherwise, the notification will not be handled.
+	/// </param>
+	/// <returns></returns>
+	bool SendToastNotification(AppNotification appNotification);
+
+	/// <summary>
+	/// Handles a toast notification activation on app launch.
+	/// </summary>
+	/// <param name="args"></param>
+	void ProcessLaunchActivationArgs(AppNotificationActivatedEventArgs args);
+}
---
+++ src/Whim/Notification/NotificationManager.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Windows.AppNotifications;
+
+namespace Whim;
+
+internal class NotificationManager : INotificationManager
+{
+	private bool _disposedValue;
+	private bool _initialized;
+
+	private readonly IContext _context;
+	private readonly Dictionary<string, Action<AppNotificationActivatedEventArgs>> _notificationHandlers = new();
+
+	public NotificationManager(IContext context)
+	{
+		_context = context;
+	}
+
+	public void Initialize()
+	{
+		// To ensure all Notification handling happens in this process instance, register for
+		// NotificationInvoked before calling Register(). Without this a new process will
+		// be launched to handle the notification.
+		AppNotificationManager notificationManager = AppNotificationManager.Default;
+		notificationManager.NotificationInvoked += AppNotificationManager_NotificationInvoked;
+		notificationManager.Register();
+		_initialized = true;
+	}
+
+	private void AppNotificationManager_NotificationInvoked(
+		AppNotificationManager sender,
+		AppNotificationActivatedEventArgs args
+	)
+	{
+		try
+		{
+			string notificationId = args.Arguments[INotificationManager.NotificationIdKey];
+			_notificationHandlers[notificationId]?.Invoke(args);
+		}
+		catch (Exception e)
+		{
+			_context.HandleUncaughtException(nameof(AppNotificationManager_NotificationInvoked), e);
+		}
+	}
+
+	public void ProcessLaunchActivationArgs(AppNotificationActivatedEventArgs args)
+	{
+		AppNotificationManager_NotificationInvoked(AppNotificationManager.Default, args);
+	}
+
+	public void Register(string notificationId, Action<AppNotificationActivatedEventArgs> notificationReceived)
+	{
+		_notificationHandlers.Add(notificationId, notificationReceived);
+	}
+
+	public void Unregister(string notificationId)
+	{
+		_notificationHandlers.Remove(notificationId);
+	}
+
+	public bool SendToastNotification(AppNotification appNotification)
+	{
+		if (!_initialized)
+		{
+			throw new InvalidOperationException(
+				"Notification manager must be initialized before sending notifications."
+			);
+		}
+
+		AppNotificationManager.Default.Show(appNotification);
+
+		return appNotification.Id != 0;
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!_disposedValue)
+		{
+			if (_initialized)
+			{
+				AppNotificationManager.Default.Unregister();
+			}
+
+			_disposedValue = true;
+		}
+	}
+
+	// Free unmanaged resources
+	~NotificationManager()
+	{
+		// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		Dispose(disposing: false);
+	}
+
+	public void Dispose()
+	{
+		// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+}
