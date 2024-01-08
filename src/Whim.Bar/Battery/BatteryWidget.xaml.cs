314fa1b3229d8bfc86167e2a330923a0546d3644
Isaac Daly <isaac.daly@outlook.com>
2024-01-09 00:31:42 +1300

    Add battery widget

---
+++ src/Whim.Bar/Battery/BatteryWidget.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Whim.Bar;
+
+/// <summary>
+/// Interaction logic for ActiveLayoutWidget.xaml
+/// </summary>
+public partial class BatteryWidget : UserControl
+{
+	internal BatteryWidgetViewModel ViewModel { get; }
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ActiveLayoutWidget"/> class.
+	/// </summary>
+	public BatteryWidget(IContext config, IMonitor monitor)
+	{
+		ViewModel = new BatteryWidgetViewModel();
+		UIElementExtensions.InitializeComponent(this, "Whim.Bar", "Battery/BatteryWidget");
+	}
+
+	/// <summary>
+	/// Creates a new bar component which contains the active layout widget.
+	/// </summary>
+	public static BarComponent CreateComponent()
+	{
+		return new BarComponent((context, monitor, window) => new BatteryWidget(context, monitor));
+	}
+}
