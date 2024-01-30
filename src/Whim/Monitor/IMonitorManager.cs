d6cf833de06333b1c4f409c5d2359b3346c30392
Isaac Daly <isaac.daly@outlook.com>
2024-01-30 20:44:05 +1100

    Remove unnecessary access modifiers

--- src/Whim/Monitor/IMonitorManager.cs
+++ src/Whim/Monitor/IMonitorManager.cs
@@ -11,44 +11,44 @@ public interface IMonitorManager : IEnumerable<IMonitor>, IDisposable
 	/// <summary>
 	/// The number of <see cref="IMonitor"/>s contained by <see cref="IMonitorManager"/>.
 	/// </summary>
-	public int Length { get; }
+	int Length { get; }
 
 	/// <summary>
 	/// The currently active <see cref="IMonitor"/>.
 	/// </summary>
-	public IMonitor ActiveMonitor { get; }
+	IMonitor ActiveMonitor { get; }
 
 	/// <summary>
 	/// The primary <see cref="IMonitor"/>.
 	/// </summary>
-	public IMonitor PrimaryMonitor { get; }
+	IMonitor PrimaryMonitor { get; }
 
 	/// <summary>
 	/// Initialize the windows event hooks.
 	/// </summary>
-	public void Initialize();
+	void Initialize();
 
 	/// <summary>
 	/// Returns the <see cref="IMonitor"/> at the given <i>x</i> and <i>y</i> coordinates.
 	/// </summary>
 	/// <param name="point">Point defined in terms of the real monitor coordinates.</param>
 	/// <returns></returns>
-	public IMonitor GetMonitorAtPoint(IPoint<int> point);
+	IMonitor GetMonitorAtPoint(IPoint<int> point);
 
 	/// <summary>
 	/// Event raised when the monitors handled by Whim are changed.
 	/// </summary>
-	public event EventHandler<MonitorsChangedEventArgs>? MonitorsChanged;
+	event EventHandler<MonitorsChangedEventArgs>? MonitorsChanged;
 
 	/// <summary>
 	/// Gets the monitor before the given monitor.
 	/// </summary>
 	/// <returns></returns>
-	public IMonitor GetPreviousMonitor(IMonitor monitor);
+	IMonitor GetPreviousMonitor(IMonitor monitor);
 
 	/// <summary>
 	/// Gets the monitor after the given monitor.
 	/// </summary>
 	/// <returns></returns>
-	public IMonitor GetNextMonitor(IMonitor monitor);
+	IMonitor GetNextMonitor(IMonitor monitor);
 }
