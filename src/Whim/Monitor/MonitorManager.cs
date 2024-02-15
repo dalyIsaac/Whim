c5b15aa7a085f90ac2fd304ab2a949240bb6ed77
Isaac Daly <isaac.daly@outlook.com>
2024-02-15 21:55:11 +1100

    Account for windows misreporting monitors for minimized windows

--- src/Whim/Monitor/MonitorManager.cs
+++ src/Whim/Monitor/MonitorManager.cs
@@ -74,7 +74,19 @@ internal class MonitorManager : IInternalMonitorManager, IMonitorManager
 
 	public void OnWindowFocused(IWindow? window)
 	{
-		HWND hwnd = window?.Handle ?? _internalContext.CoreNativeManager.GetForegroundWindow();
+		if (window is not null)
+		{
+			Logger.Debug($"Focusing window {window}");
+			if (_context.Butler.GetMonitorForWindow(window) is IMonitor monitor)
+			{
+				Logger.Debug($"Setting active monitor to {monitor}");
+				ActiveMonitor = monitor;
+				LastWhimActiveMonitor = monitor;
+				return;
+			}
+		}
+
+		HWND hwnd = _internalContext.CoreNativeManager.GetForegroundWindow();
 		Logger.Debug($"Focusing hwnd {hwnd}");
 
 		if (hwnd.IsNull)
@@ -93,13 +105,7 @@ internal class MonitorManager : IInternalMonitorManager, IMonitorManager
 			if (monitor._hmonitor.Equals(hMONITOR))
 			{
 				Logger.Debug($"Setting active monitor to {monitor}");
-
 				ActiveMonitor = monitor;
-
-				if (window is not null)
-				{
-					LastWhimActiveMonitor = monitor;
-				}
 				break;
 			}
 		}
