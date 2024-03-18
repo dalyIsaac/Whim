8796ca568c38f44d14d97425619861c479f50d6d
Isaac Daly <isaac.daly@outlook.com>
2024-03-18 19:35:47 +1100

    FilterManager locks

--- src/Whim/Filter/FilterManager.cs
+++ src/Whim/Filter/FilterManager.cs
@@ -31,35 +31,42 @@ internal class FilterManager : IFilterManager
 		_filters.Clear();
 	}
 
-	public bool ShouldBeIgnored(IWindow window) =>
-		_ignoreWindowClasses.Contains(window.WindowClass.ToLower())
-		|| (
-			window.ProcessFileName is string processFileName
-			&& _ignoreProcessFileNames.Contains(processFileName.ToLower())
-		)
-		|| _ignoreTitles.Contains(window.Title.ToLower())
-		|| _filters.Any(f => f(window));
+	public bool ShouldBeIgnored(IWindow window)
+	{
+		using Lock _ = new();
+		return _ignoreWindowClasses.Contains(window.WindowClass.ToLower())
+			|| (
+				window.ProcessFileName is string processFileName
+				&& _ignoreProcessFileNames.Contains(processFileName.ToLower())
+			)
+			|| _ignoreTitles.Contains(window.Title.ToLower())
+			|| _filters.Any(f => f(window));
+	}
 
 	public IFilterManager AddWindowClassFilter(string windowClass)
 	{
+		using Lock _ = new();
 		_ignoreWindowClasses.Add(windowClass.ToLower());
 		return this;
 	}
 
 	public IFilterManager AddProcessFileNameFilter(string processFileName)
 	{
+		using Lock _ = new();
 		_ignoreProcessFileNames.Add(processFileName.ToLower());
 		return this;
 	}
 
 	public IFilterManager AddTitleFilter(string title)
 	{
+		using Lock _ = new();
 		_ignoreTitles.Add(title.ToLower());
 		return this;
 	}
 
 	public IFilterManager AddTitleMatchFilter(string title)
 	{
+		using Lock _ = new();
 		Regex regex = new(title);
 		_filters.Add(window => regex.IsMatch(window.Title));
 		return this;
