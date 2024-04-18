0695ff7ea8f2d309909b2d7102175f4d04ec3c31
Isaac Daly <isaac.daly@outlook.com>
2024-04-18 20:40:57 +1000

    StoreTests

---
+++ src/Whim.Tests/Store/StoreTests.cs
@@ -0,0 +1,37 @@
+using System;
+using NSubstitute;
+using Whim.TestUtils;
+using Xunit;
+
+namespace Whim.Tests;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope")]
+public class StoreTests
+{
+	[Theory, AutoSubstituteData]
+	internal void Initialize(IContext ctx, IInternalContext internalCtx)
+	{
+		// Given
+		Store sut = new(ctx, internalCtx);
+
+		// When we initialize
+		sut.Initialize();
+
+		// Then the MonitorSlice has initialized
+		internalCtx.WindowMessageMonitor.WorkAreaChanged += Arg.Any<EventHandler<WindowMessageMonitorEventArgs>>();
+	}
+
+	[Theory, AutoSubstituteData]
+	internal void Dispose(IContext ctx, IInternalContext internalCtx)
+	{
+		// Given
+		Store sut = new(ctx, internalCtx);
+
+		// When we initialize and then dispose
+		sut.Initialize();
+		sut.Dispose();
+
+		// Then the MonitorSlice has initialized
+		internalCtx.WindowMessageMonitor.WorkAreaChanged -= Arg.Any<EventHandler<WindowMessageMonitorEventArgs>>();
+	}
+}
