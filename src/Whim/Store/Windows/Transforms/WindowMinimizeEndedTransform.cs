77275d8dab974a5fecb4f3e80a654c0d2a665008
Isaac Daly <isaac.daly@outlook.com>
2024-04-20 21:11:43 +1000

    Update transforms

--- src/Whim/Store/Windows/Transforms/WindowMinimizeEndedTransform.cs
+++ src/Whim/Store/Windows/Transforms/WindowMinimizeEndedTransform.cs
@@ -1,11 +1,14 @@
+using DotNext;
+
 namespace Whim;
 
 internal record WindowMinimizeEndedTransform(IWindow Window) : Transform
 {
-	internal override void Execute(IContext ctx, IInternalContext internalCtx)
+	internal override Result<Empty> Execute(IContext ctx, IInternalContext internalCtx)
 	{
 		WindowMinimizeEndedEventArgs args = new() { Window = Window };
 		internalCtx.ButlerEventHandlers.OnWindowMinimizeEnd(args);
 		ctx.Store.WindowSlice.QueueEvent(args);
+		return Empty.Result;
 	}
 }
--- src/Whim/Store/Windows/Transforms/WindowMinimizeStartedTransform.cs
+++ src/Whim/Store/Windows/Transforms/WindowMinimizeStartedTransform.cs
@@ -1,8 +1,10 @@
+using DotNext;
+
 namespace Whim;
 
 internal record WindowMinimizeStartedTransform(IWindow Window) : Transform
 {
-	internal override void Execute(IContext ctx, IInternalContext internalCtx)
+	internal override Result<Empty> Execute(IContext ctx, IInternalContext internalCtx)
 	{
 		WindowSlice slice = ctx.Store.WindowSlice;
 
@@ -10,5 +12,6 @@ internal record WindowMinimizeStartedTransform(IWindow Window) : Transform
 		internalCtx.ButlerEventHandlers.OnWindowMinimizeStart(args);
 
 		slice.QueueEvent(args);
+		return Empty.Result;
 	}
 }
