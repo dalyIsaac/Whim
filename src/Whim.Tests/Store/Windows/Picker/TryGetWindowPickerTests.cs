3009a18918027124b6ec2caf4e9fb9845be3d06c
Isaac Daly <isaac.daly@outlook.com>
2024-04-24 23:32:36 +1000

    TryGetWindowPickerTests

---
+++ src/Whim.Tests/Store/Windows/Picker/TryGetWindowPickerTests.cs
@@ -0,0 +1,37 @@
+using Whim.TestUtils;
+using Windows.Win32.Foundation;
+using Xunit;
+
+namespace Whim;
+
+public class TryGetWindowPickerTests
+{
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void CannotFindWindow(IContext ctx)
+	{
+		// Given the window cannot be found
+		HWND hwnd = (HWND)1;
+		TryGetWindowPicker sut = new(hwnd);
+
+		// When we execute the picker
+		var result = ctx.Store.Pick(sut);
+
+		// Then we get an unsuccessful response
+		Assert.False(result.IsSuccessful);
+	}
+
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void FoundWindow(IContext ctx, IWindow window)
+	{
+		// Given the window can be found
+		ctx.Store.WindowSlice.Windows = ctx.Store.WindowSlice.Windows.Add(window.Handle, window);
+		TryGetWindowPicker sut = new(window.Handle);
+
+		// When
+		var result = ctx.Store.Pick(sut);
+
+		// Then
+		Assert.True(result.IsSuccessful);
+		Assert.Equal(window, result.Value);
+	}
+}
