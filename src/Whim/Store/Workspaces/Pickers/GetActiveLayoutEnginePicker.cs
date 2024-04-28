61ef5a1f83a15cecff6d0c2e6f330a4c94824bd9
Isaac Daly <isaac.daly@outlook.com>
2024-04-28 11:57:49 +1000

    GetActiveLayoutEnginePicker now uses BaseWorkspacePicker

--- src/Whim/Store/Workspaces/Pickers/GetActiveLayoutEnginePicker.cs
+++ src/Whim/Store/Workspaces/Pickers/GetActiveLayoutEnginePicker.cs
@@ -1,18 +1,14 @@
+using System;
+
 namespace Whim;
 
 /// <summary>
 /// Get the active layout engine in the provided workspace.
 /// </summary>
-/// <param name="Workspace">The workspace to get the active layout engine for.</param>
-public record GetActiveLayoutEnginePicker(ImmutableWorkspace Workspace) : Picker<ILayoutEngine>
+/// <param name="WorkspaceId"></param>
+public record GetActiveLayoutEnginePicker(Guid WorkspaceId) : BaseWorkspacePicker<ILayoutEngine>(WorkspaceId)
 {
-	internal override ILayoutEngine Execute(
-		IContext ctx,
-		IInternalContext internalCtx,
-		IRootSector rootSelector
-	)
-	{
-		int layoutEngineIdx = Workspace.ActiveLayoutEngineIndex;
-		return Workspace.LayoutEngines[layoutEngineIdx];
-	}
+	/// <inheritdoc/>
+	protected override ILayoutEngine Operation(ImmutableWorkspace workspace) =>
+		workspace.LayoutEngines[workspace.ActiveLayoutEngineIndex];
 }
