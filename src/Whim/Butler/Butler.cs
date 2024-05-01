d6e656d8e4f133d0866873e1c23a024282cdf9a9
Isaac Daly <isaac.daly@outlook.com>
2024-05-01 21:46:14 +1000

    Remove ButlerChores implementation

--- src/Whim/Butler/Butler.cs
+++ src/Whim/Butler/Butler.cs
@@ -9,7 +9,6 @@ internal partial class Butler : IButler
 	private readonly IContext _context;
 	private readonly IInternalContext _internalContext;
 
-	private readonly IButlerChores _chores;
 	private bool _initialized;
 
 	private IButlerPantry _pantry;
@@ -34,7 +33,6 @@ internal partial class Butler : IButler
 		_internalContext = internalContext;
 
 		_pantry = new ButlerPantry(_context);
-		_chores = new ButlerChores(_context);
 	}
 
 	public event EventHandler<RouteEventArgs>? WindowRouted;
@@ -105,37 +103,42 @@ internal partial class Butler : IButler
 	}
 
 	#region Chores
-	public void Activate(IWorkspace workspace, IMonitor? monitor = null) => _chores.Activate(workspace, monitor);
+	public void Activate(IWorkspace workspace, IMonitor? monitor = null) =>
+		_context.Store.Dispatch(new ActivateWorkspaceTransform(workspace, monitor));
 
 	public void ActivateAdjacent(IMonitor? monitor = null, bool reverse = false, bool skipActive = false) =>
-		_chores.ActivateAdjacent(monitor, reverse, skipActive);
+		_context.Store.Dispatch(new ActivateAdjacentTransform(monitor, reverse, skipActive));
 
-	public void LayoutAllActiveWorkspaces() => _chores.LayoutAllActiveWorkspaces();
+	public void LayoutAllActiveWorkspaces() => _context.Store.Dispatch(new LayoutAllActiveWorkspacesTransform());
 
-	public void FocusMonitorDesktop(IMonitor monitor) => _chores.FocusMonitorDesktop(monitor);
-
-	public void MergeWorkspaceWindows(IWorkspace workspaceToDelete, IWorkspace workspaceMergeTarget) =>
-		_chores.MergeWorkspaceWindows(workspaceToDelete, workspaceMergeTarget);
+	public void FocusMonitorDesktop(IMonitor monitor) =>
+		_context.Store.Dispatch(new FocusMonitorDesktopTransform(monitor));
 
 	public bool MoveWindowEdgesInDirection(Direction edges, IPoint<int> pixelsDeltas, IWindow? window = null) =>
-		_chores.MoveWindowEdgesInDirection(edges, pixelsDeltas, window);
+		_context.Store.Dispatch(new MoveWindowEdgesInDirectionTransform(edges, pixelsDeltas, window)).IsSuccessful;
 
 	public void MoveWindowToAdjacentWorkspace(IWindow? window = null, bool reverse = false, bool skipActive = false) =>
-		_chores.MoveWindowToAdjacentWorkspace(window, reverse, skipActive);
-
-	public void MoveWindowToWorkspace(IWorkspace workspace, IWindow? window = null) =>
-		_chores.MoveWindowToWorkspace(workspace, window);
+		_context.Store.Dispatch(new MoveWindowToAdjacentWorkspaceTransform(window, reverse, skipActive));
 
 	public void MoveWindowToMonitor(IMonitor monitor, IWindow? window = null) =>
-		_chores.MoveWindowToMonitor(monitor, window);
+		_context.Store.Dispatch(new MoveWindowToMonitorTransform(monitor, window));
 
-	public void MoveWindowToNextMonitor(IWindow? window = null) => _chores.MoveWindowToNextMonitor(window);
+	public void MoveWindowToPreviousMonitor(IWindow? window = null) =>
+		_context.Store.Dispatch(new MoveWindowToAdjacentMonitorTransform(window, Reverse: true));
 
-	public void MoveWindowToPreviousMonitor(IWindow? window = null) => _chores.MoveWindowToPreviousMonitor(window);
+	public void MoveWindowToNextMonitor(IWindow? window = null) =>
+		_context.Store.Dispatch(new MoveWindowToAdjacentMonitorTransform(window, Reverse: false));
 
-	public void MoveWindowToPoint(IWindow window, IPoint<int> point) => _chores.MoveWindowToPoint(window, point);
+	public void MoveWindowToPoint(IWindow window, IPoint<int> point) =>
+		_context.Store.Dispatch(new MoveWindowToPointTransform(window, point));
+
+	public void MoveWindowToWorkspace(IWorkspace targetWorkspace, IWindow? window = null) =>
+		_context.Store.Dispatch(new MoveWindowToWorkspaceTransform(targetWorkspace, window));
+
+	public void MergeWorkspaceWindows(IWorkspace source, IWorkspace target) =>
+		_context.Store.Dispatch(new MergeWorkspaceWindowsTransform(source, target));
 
 	public void SwapWorkspaceWithAdjacentMonitor(IWorkspace? workspace = null, bool reverse = false) =>
-		_chores.SwapWorkspaceWithAdjacentMonitor(workspace, reverse);
+		_context.Store.Dispatch(new SwapWorkspaceWithAdjacentMonitorTransform(workspace, reverse));
 	#endregion
 }
