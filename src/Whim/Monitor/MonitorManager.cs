45a99589f27879d1ed46fd4baa01598fcfb6349f
Isaac Daly <isaac.daly@outlook.com>
2024-05-12 17:35:09 +1000

    Remove unused items from MonitorManager

--- src/Whim/Monitor/MonitorManager.cs
+++ src/Whim/Monitor/MonitorManager.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
-using System.Threading.Tasks;
-using Windows.Win32.Foundation;
 using Windows.Win32.Graphics.Gdi;
 
 namespace Whim;
@@ -15,14 +12,7 @@ namespace Whim;
 internal class MonitorManager : IInternalMonitorManager, IMonitorManager
 {
 	private readonly IContext _context;
-	private readonly IInternalContext _internalContext;
 
-	/// <summary>
-	/// The <see cref="IMonitor"/>s of the computer.
-	/// This is initialized to make the compiler happy - we should never have a case where there's
-	/// zero <see cref="IMonitor"/>s, as the constructor should throw.
-	/// </summary>
-	private Monitor[] _monitors = Array.Empty<Monitor>();
 	private bool _disposedValue;
 
 	public IMonitor ActiveMonitor => _context.Store.Pick(Pickers.GetActiveMonitor());
@@ -46,27 +36,14 @@ internal class MonitorManager : IInternalMonitorManager, IMonitorManager
 	/// When no monitors are found, or there is no primary monitor.
 	/// </exception>
 	/// <param name="context"></param>
-	/// <param name="internalContext"></param>
-	public MonitorManager(IContext context, IInternalContext internalContext)
+	public MonitorManager(IContext context)
 	{
 		_context = context;
-		_internalContext = internalContext;
-
-		// Get the monitors.
-		_monitors = GetCurrentMonitors();
-
-		// Get the initial active monitor
-		IMonitor? primaryMonitor =
-			(_monitors?.FirstOrDefault(m => m.IsPrimary)) ?? throw new Exception("No primary monitor found.");
-		ActiveMonitor = primaryMonitor;
-		PrimaryMonitor = primaryMonitor;
-		LastWhimActiveMonitor = primaryMonitor;
 	}
 
 	public void Initialize()
 	{
 		_context.Store.MonitorEvents.MonitorsChanged += MonitorSector_MonitorsChanged;
-		_internalContext.MouseHook.MouseLeftButtonUp += MouseHook_MouseLeftButtonUp;
 	}
 
 	private void MonitorSector_MonitorsChanged(object? sender, MonitorsChangedEventArgs e) =>
@@ -75,13 +52,6 @@ internal class MonitorManager : IInternalMonitorManager, IMonitorManager
 	public void ActivateEmptyMonitor(IMonitor monitor) =>
 		_context.Store.Dispatch(new ActivateEmptyMonitorTransform(monitor.Handle));
 
-	private void MouseHook_MouseLeftButtonUp(object? sender, MouseEventArgs e)
-	{
-		IMonitor monitor = GetMonitorAtPoint(e.Point);
-		Logger.Debug($"Mouse left button up on {monitor}");
-		ActiveMonitor = monitor;
-	}
-
 	public IMonitor GetMonitorAtPoint(IPoint<int> point) => _context.Store.Pick(Pickers.GetMonitorAtPoint(point)).Value;
 
 	public IMonitor GetPreviousMonitor(IMonitor monitor) =>
@@ -89,8 +59,6 @@ internal class MonitorManager : IInternalMonitorManager, IMonitorManager
 
 	public IMonitor GetNextMonitor(IMonitor monitor) =>
 		_context.Store.Pick(Pickers.GetAdjacentMonitor(monitor.Handle, reverse: false, getFirst: true)).Value;
-
-	public IMonitor? GetMonitorByHandle(HMONITOR hmonitor) => _monitors.FirstOrDefault(m => m.Handle == hmonitor);
 
 	protected virtual void Dispose(bool disposing)
 	{
@@ -102,9 +70,6 @@ internal class MonitorManager : IInternalMonitorManager, IMonitorManager
 
 				// dispose managed state (managed objects)
 				_context.Store.MonitorEvents.MonitorsChanged -= MonitorSector_MonitorsChanged;
-
-				_internalContext.MouseHook.MouseLeftButtonUp -= MouseHook_MouseLeftButtonUp;
-				_internalContext.Dispose();
 			}
 
 			// free unmanaged resources (unmanaged objects) and override finalizer
---
+++ src/Whim/Store/RootSector/Transforms/MouseLeftButtonUpTransform.cs
@@ -0,0 +1,20 @@
+using DotNext;
+
+namespace Whim;
+
+internal record MouseLeftButtonUpTransform(IPoint<int> Point) : Transform
+{
+	internal override Result<Unit> Execute(
+		IContext ctx,
+		IInternalContext internalCtx,
+		MutableRootSector mutableRootSector
+	)
+	{
+		if (ctx.Store.Pick(Pickers.GetMonitorAtPoint(Point)).TryGet(out IMonitor monitor))
+		{
+			mutableRootSector.MonitorSector.ActiveMonitorHandle = monitor.Handle;
+		}
+
+		return Unit.Result;
+	}
+}
