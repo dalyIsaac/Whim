176b7b4e5cfef3b546b3b1c172ba3b8c984d4d8e
Isaac Daly <isaac.daly@outlook.com>
2024-05-13 20:50:10 +1000

    Actually MonitorsChangedTransformTests

--- src/Whim.Tests/Store/MonitorSector/Transforms/MonitorsChangedTransformTests.cs
+++ src/Whim.Tests/Store/MonitorSector/Transforms/MonitorsChangedTransformTests.cs
@@ -1,39 +1,112 @@
-using System.Collections.Immutable;
+using System.Linq;
 using Whim.TestUtils;
+using Windows.Win32.Foundation;
 using Windows.Win32.Graphics.Gdi;
 using Xunit;
 
 namespace Whim.Tests;
 
-public class ActivateEmptyMonitorTransformTests
+public class MonitorsChangedTransformTests
 {
+	private static readonly (RECT, HMONITOR) RightMonitorSetup = (
+		new RECT()
+		{
+			left = 1920,
+			top = 0,
+			right = 3840,
+			bottom = 1080
+		},
+		(HMONITOR)1
+	);
+
+	private static readonly (RECT, HMONITOR) LeftTopMonitorSetup = (
+		new RECT()
+		{
+			left = 0,
+			top = 0,
+			right = 1920,
+			bottom = 1080
+		},
+		(HMONITOR)2
+	);
+
+	private static readonly (RECT, HMONITOR) LeftBottomMonitorSetup = (
+		new RECT()
+		{
+			left = 0,
+			top = 1080,
+			right = 1920,
+			bottom = 2160
+		},
+		(HMONITOR)3
+	);
+
+	private static Assert.RaisedEvent<MonitorsChangedEventArgs> DispatchTransformEvent(IContext ctx) =>
+		Assert.Raises<MonitorsChangedEventArgs>(
+			h => ctx.Store.MonitorEvents.MonitorsChanged += h,
+			h => ctx.Store.MonitorEvents.MonitorsChanged -= h,
+			() => ctx.Store.Dispatch(new MonitorsChangedTransform())
+		);
+
 	[Theory, AutoSubstituteData<StoreCustomization>]
-	internal void MonitorNotFound(IContext ctx, MutableRootSector mutableRootSector)
+	internal void MonitorsRemoved(IContext ctx, IInternalContext internalCtx)
 	{
-		// Given the monitor does not exist in the store
-		ActivateEmptyMonitorTransform sut = new((HMONITOR)123);
+		// Given we've populated monitors
+		MonitorTestUtils.SetupMultipleMonitors(
+			internalCtx,
+			new[] { RightMonitorSetup, LeftTopMonitorSetup, LeftBottomMonitorSetup }
+		);
+		ctx.Store.Dispatch(new MonitorsChangedTransform());
 
-		// When we execute the transform
-		ctx.Store.Dispatch(sut);
+		// When a monitor is removed
+		MonitorTestUtils.SetupMultipleMonitors(internalCtx, new[] { RightMonitorSetup, LeftTopMonitorSetup });
 
-		// Then the ActiveMonitorIndex was not updated
-		Assert.Equal((HMONITOR)0, mutableRootSector.MonitorSector.ActiveMonitorHandle);
+		// Then the resulting event will have a monitor removed
+		var raisedEvent = DispatchTransformEvent(ctx);
+
+		Assert.Empty(raisedEvent.Arguments.AddedMonitors);
+		Assert.Single(raisedEvent.Arguments.RemovedMonitors);
+		Assert.Equal(2, raisedEvent.Arguments.UnchangedMonitors.Count());
 	}
 
 	[Theory, AutoSubstituteData<StoreCustomization>]
-	internal void MonitorFound(IContext ctx, MutableRootSector mutableRootSector, IMonitor monitor, IMonitor monitor1)
+	internal void MonitorsAdded(IContext ctx, IInternalContext internalCtx)
 	{
-		// Given the store contains multiple monitors
-		HMONITOR handle = (HMONITOR)123;
-		mutableRootSector.MonitorSector.Monitors = ImmutableArray.Create(monitor, monitor1);
-		mutableRootSector.MonitorSector.ActiveMonitorHandle = handle;
+		// Given we've populated monitors
+		MonitorTestUtils.SetupMultipleMonitors(internalCtx, new[] { RightMonitorSetup, LeftTopMonitorSetup });
+		ctx.Store.Dispatch(new MonitorsChangedTransform());
 
-		ActivateEmptyMonitorTransform sut = new(handle);
+		// When a monitor is added
+		MonitorTestUtils.SetupMultipleMonitors(
+			internalCtx,
+			new[] { RightMonitorSetup, LeftTopMonitorSetup, LeftBottomMonitorSetup }
+		);
 
-		// When we execute the transform
-		ctx.Store.Dispatch(sut);
+		// Then the resulting event will have a monitor added
+		var raisedEvent = DispatchTransformEvent(ctx);
 
-		// Then the ActiveMonitorIndex was updated
-		Assert.Equal(handle, mutableRootSector.MonitorSector.ActiveMonitorHandle);
+		Assert.Single(raisedEvent.Arguments.AddedMonitors);
+		Assert.Empty(raisedEvent.Arguments.RemovedMonitors);
+		Assert.Equal(2, raisedEvent.Arguments.UnchangedMonitors.Count());
+	}
+
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void MonitorsUnchanged(IContext ctx, IInternalContext internalCtx)
+	{
+		// Given there are no changes in the monitors.
+		MonitorTestUtils.SetupMultipleMonitors(
+			internalCtx,
+			new[] { RightMonitorSetup, LeftTopMonitorSetup, LeftBottomMonitorSetup }
+		);
+
+		// When we dispatch the same transform twice, the first from a clean store
+		ctx.Store.Dispatch(new MonitorsChangedTransform());
+
+		// Then the second dispatch should receive all monitors as unchanged.
+		var raisedEvent = DispatchTransformEvent(ctx);
+
+		Assert.Empty(raisedEvent.Arguments.AddedMonitors);
+		Assert.Empty(raisedEvent.Arguments.RemovedMonitors);
+		Assert.Equal(3, raisedEvent.Arguments.UnchangedMonitors.Count());
 	}
 }
