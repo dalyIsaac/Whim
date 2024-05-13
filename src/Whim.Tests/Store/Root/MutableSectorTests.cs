2aaac9115c9edf96b50883fc860a5f7aff48c6c4
Isaac Daly <isaac.daly@outlook.com>
2024-05-14 02:35:42 +1000

    MutableSectorTests

---
+++ src/Whim.Tests/Store/Root/MutableSectorTests.cs
@@ -0,0 +1,24 @@
+using System;
+using NSubstitute;
+using Whim.TestUtils;
+using Xunit;
+
+namespace Whim.Tests;
+
+public class MutableRootSectorTests
+{
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void Initialize_Dispose(IContext ctx, IInternalContext internalCtx)
+	{
+		// Given
+		MutableRootSector sut = new(ctx, internalCtx);
+
+		// When
+		sut.Initialize();
+		sut.Dispose();
+
+		// Then
+		internalCtx.WindowMessageMonitor.DisplayChanged += Arg.Any<EventHandler<WindowMessageMonitorEventArgs>>();
+		internalCtx.WindowMessageMonitor.DisplayChanged -= Arg.Any<EventHandler<WindowMessageMonitorEventArgs>>();
+	}
+}
