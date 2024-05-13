01eaddf3eae271b29265307841793d954b2c666a
Isaac Daly <isaac.daly@outlook.com>
2024-05-14 02:27:51 +1000

    RootSectorTests

---
+++ src/Whim.Tests/Store/Root/RootSectorTests.cs
@@ -0,0 +1,26 @@
+using NSubstitute;
+using System;
+using Whim.TestUtils;
+using Xunit;
+
+namespace Whim.Tests;
+
+public class RootSectorTests
+{
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void Initialize_Dispose(IContext ctx, IInternalContext internalCtx)
+	{
+		// Given
+		RootSector sut = new(ctx, internalCtx);
+
+		// When
+		sut.Initialize();
+		sut.Dispose();
+
+		// Then
+		internalCtx.MouseHook.MouseLeftButtonUp += Arg.Any<EventHandler<MouseEventArgs>>();
+		internalCtx.MouseHook.MouseLeftButtonDown += Arg.Any<EventHandler<MouseEventArgs>>();
+		internalCtx.MouseHook.MouseLeftButtonUp -= Arg.Any<EventHandler<MouseEventArgs>>();
+		internalCtx.MouseHook.MouseLeftButtonDown -= Arg.Any<EventHandler<MouseEventArgs>>();
+	}
+}
