2ef7b9256d9cae70129391731bd8e82e673fa585
Isaac Daly <isaac.daly@outlook.com>
2024-05-14 21:33:14 +1000

    WindowMoveStartedTransform

---
+++ src/Whim/Store/WindowSector/Transforms/WindowMoveStartedTransform.cs
@@ -0,0 +1,34 @@
+using DotNext;
+
+namespace Whim;
+
+internal record WindowMoveStartedTransform(IWindow Window) : Transform
+{
+	internal override Result<Unit> Execute(
+		IContext ctx,
+		IInternalContext internalCtx,
+		MutableRootSector mutableRootSector
+	)
+	{
+		WindowSector windowSector = mutableRootSector.WindowSector;
+
+		windowSector.IsMovingWindow = true;
+
+		IPoint<int>? cursorPoint = null;
+		if (windowSector.IsLeftMouseButtonDown && internalCtx.CoreNativeManager.GetCursorPos(out IPoint<int> point))
+		{
+			cursorPoint = point;
+		}
+
+		windowSector.QueueEvent(
+			new WindowMoveStartedEventArgs()
+			{
+				Window = Window,
+				CursorDraggedPoint = cursorPoint,
+				MovedEdges = WindowUtils.GetMovedEdges(ctx, Window)?.MovedEdges
+			}
+		);
+
+		return Unit.Result;
+	}
+}
--- src/Whim/Store/WindowSector/WindowUtils.cs
+++ src/Whim/Store/WindowSector/WindowUtils.cs
@@ -8,4 +8,80 @@ internal class WindowUtils
 	[Obsolete("Temporary until the Butler is replaced")]
 	public static IWindow? GetWindow(MutableRootSector rootSector, HWND handle) =>
 		rootSector.WindowSector.Windows.TryGetValue(handle, out IWindow? w) ? w : null;
+
+	/// <summary>
+	/// Tries to move the given window's edges in the direction of the mouse movement.
+	/// </summary>
+	/// <param name="ctx"></param>
+	/// <param name="window"></param>
+	/// <returns></returns>
+	public static (Direction MovedEdges, IPoint<int> MovedPoint)? GetMovedEdges(IContext ctx, IWindow window)
+	{
+		Logger.Debug("Trying to move window edges in direction of mouse movement");
+		IWorkspace? workspace = ctx.Butler.Pantry.GetWorkspaceForWindow(window);
+		if (workspace is null)
+		{
+			Logger.Debug($"Could not find workspace for window {window}, failed to move window edges");
+			return null;
+		}
+
+		IWindowState? windowState = workspace.TryGetWindowState(window);
+		if (windowState is null)
+		{
+			Logger.Debug($"Could not find window state for window {window}, failed to move window edges");
+			return null;
+		}
+
+		// Get the new window position.
+		IRectangle<int>? newRect = ctx.NativeManager.DwmGetWindowRectangle(window.Handle);
+		if (newRect is null)
+		{
+			Logger.Debug($"Could not get new rectangle for window {window}, failed to move window edges");
+			return null;
+		}
+
+		// Find the one or two edges to move.
+		int leftEdgeDelta = windowState.Rectangle.X - newRect.X;
+		int topEdgeDelta = windowState.Rectangle.Y - newRect.Y;
+		int rightEdgeDelta = newRect.X + newRect.Width - (windowState.Rectangle.X + windowState.Rectangle.Width);
+		int bottomEdgeDelta = newRect.Y + newRect.Height - (windowState.Rectangle.Y + windowState.Rectangle.Height);
+
+		int movedEdgeCountX = 0;
+		int movedEdgeCountY = 0;
+		int movedEdgeDeltaX = 0;
+		int movedEdgeDeltaY = 0;
+		Direction movedEdges = Direction.None;
+		if (leftEdgeDelta != 0)
+		{
+			movedEdges |= Direction.Left;
+			movedEdgeDeltaX = -leftEdgeDelta;
+			movedEdgeCountX++;
+		}
+		if (topEdgeDelta != 0)
+		{
+			movedEdges |= Direction.Up;
+			movedEdgeDeltaY = -topEdgeDelta;
+			movedEdgeCountY++;
+		}
+		if (rightEdgeDelta != 0)
+		{
+			movedEdges |= Direction.Right;
+			movedEdgeDeltaX = rightEdgeDelta;
+			movedEdgeCountX++;
+		}
+		if (bottomEdgeDelta != 0)
+		{
+			movedEdges |= Direction.Down;
+			movedEdgeDeltaY = bottomEdgeDelta;
+			movedEdgeCountY++;
+		}
+
+		if (movedEdgeCountX > 1 || movedEdgeCountY > 1)
+		{
+			Logger.Debug($"Window {window} moved more than one edge in the same axis, failed to move window edges");
+			return null;
+		}
+
+		return (movedEdges, new Point<int>() { X = movedEdgeDeltaX, Y = movedEdgeDeltaY, });
+	}
 }
