7ca45b65b07aef7828d02266d565a2ed3e72dce0
Isaac Daly <isaac.daly@outlook.com>
2024-05-23 21:32:33 +1000

    MoveWindowToMonitorTransform tests

---
+++ src/Whim.Tests/Store/MapSector/Transforms/MoveWindowToAdjacentMonitorTransformTests.cs
@@ -0,0 +1,55 @@
+using System.Collections.Immutable;
+using System.Threading;
+using Whim.TestUtils;
+using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Gdi;
+using Xunit;
+using static Whim.TestUtils.MapTestUtils;
+
+namespace Whim.Tests;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope")]
+public class MoveWindowToAdjacentMonitorTransformTests
+{
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void NoAdjacentMonitors(IContext ctx, MutableRootSector rootSector)
+	{
+		// Given there are no adjacent monitors
+		IMonitor monitor = CreateMonitor((HMONITOR)10);
+		IWindow window = CreateWindow((HWND)10);
+
+		rootSector.MonitorSector.Monitors = ImmutableArray.Create(monitor);
+		rootSector.MonitorSector.ActiveMonitorHandle = monitor.Handle;
+		AddWindowToSector(rootSector, window);
+
+		MoveWindowToAdjacentMonitorTransform sut = new(window.Handle);
+
+		// When we dispatch the transform
+		var result = ctx.Store.Dispatch(sut);
+
+		// Then
+		Assert.False(result.IsSuccessful);
+	}
+
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void Success(IContext ctx, MutableRootSector rootSector)
+	{
+		// Given there are two adjacent monitors
+		IMonitor originalMonitor = CreateMonitor((HMONITOR)10);
+		IMonitor newMonitor = CreateMonitor((HMONITOR)11);
+
+		IWindow window = CreateWindow((HWND)10);
+
+		PopulateThreeWayMap(ctx, rootSector, originalMonitor, CreateWorkspace(), window);
+		PopulateMonitorWorkspaceMap(ctx, rootSector, newMonitor, CreateWorkspace());
+		rootSector.MonitorSector.ActiveMonitorHandle = originalMonitor.Handle;
+
+		MoveWindowToAdjacentMonitorTransform sut = new(window.Handle);
+
+		// When we dispatch the transform
+		var result = ctx.Store.Dispatch(sut);
+
+		// Then
+		Assert.True(result.IsSuccessful);
+	}
+}
