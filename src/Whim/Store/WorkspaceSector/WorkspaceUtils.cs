c25c1ac5bb05ac70a2dcca786b3c190ca6f0955d
Isaac Daly <isaac.daly@outlook.com>
2024-05-23 21:24:22 +1000

    Remove unused code

--- src/Whim/Store/WorkspaceSector/WorkspaceUtils.cs
+++ src/Whim/Store/WorkspaceSector/WorkspaceUtils.cs
@@ -1,21 +1,7 @@
-using DotNext;
-
 namespace Whim;
 
 internal static class WorkspaceUtils
 {
 	public static WorkspaceId OrActiveWorkspace(this WorkspaceId WorkspaceId, IContext ctx) =>
 		ctx.WorkspaceManager.ActiveWorkspace.Id;
-
-	public static bool ContainsWorkspaceId(this IWorkspaceManager workspaceManager, WorkspaceId workspaceId)
-	{
-		foreach (IWorkspace workspace in workspaceManager)
-		{
-			if (workspace.Id.Equals(workspaceId))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
 }
