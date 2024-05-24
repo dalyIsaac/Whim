0bc6ea5bd8359e4d9c9d21c9adef729c03d6ca0a
Isaac Daly <isaac.daly@outlook.com>
2024-05-25 00:07:08 +1000

    MergeWorkspaceWindowsTransform tests

---
+++ src/Whim.Tests/Store/MapSector/Transforms/MergeWorkspaceWIndowsTransformTests.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using NSubstitute;
+using Whim.TestUtils;
+using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Gdi;
+using Xunit;
+using static Whim.TestUtils.StoreTestUtils;
+
+namespace Whim;
+
+[SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope")]
+public class MergeWorkspaceWIndowsTransformTests
+{
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void SourceWorkspaceNotFound(IContext ctx)
+	{
+		// Given
+		Guid sourceWorkspaceId = Guid.NewGuid();
+		Guid targetWorkspaceId = Guid.NewGuid();
+
+		MergeWorkspaceWindowsTransform sut = new(sourceWorkspaceId, targetWorkspaceId);
+
+		// When
+		var result = ctx.Store.Dispatch(sut);
+
+		// Then
+		Assert.False(result.IsSuccessful);
+	}
+
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void TargetWorkspaceNotFound(IContext ctx)
+	{
+		// Given
+		IWorkspace workspace = CreateWorkspace();
+		Guid targetWorkspaceId = Guid.NewGuid();
+
+		AddWorkspaceToManager(ctx, workspace);
+
+		MergeWorkspaceWindowsTransform sut = new(workspace.Id, targetWorkspaceId);
+
+		// When
+		var result = ctx.Store.Dispatch(sut);
+
+		// Then
+		Assert.False(result.IsSuccessful);
+	}
+
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void Success(IContext ctx, MutableRootSector rootSector)
+	{
+		// Given the source and target workspaces
+		IWorkspace sourceWorkspace = CreateWorkspace();
+		IWorkspace targetWorkspace = CreateWorkspace();
+
+		HMONITOR monitorHandle = (HMONITOR)1;
+		IMonitor monitor = CreateMonitor(monitorHandle);
+
+		IWindow window1 = CreateWindow((HWND)1);
+		IWindow window2 = CreateWindow((HWND)2);
+		IWindow window3 = CreateWindow((HWND)3);
+
+		sourceWorkspace.Windows.Returns(new List<IWindow>() { window1, window2, window3 });
+
+		AddWorkspaceToManager(ctx, sourceWorkspace);
+		AddWorkspaceToManager(ctx, targetWorkspace);
+
+		PopulateMonitorWorkspaceMap(ctx, rootSector, monitor, sourceWorkspace);
+
+		MergeWorkspaceWindowsTransform sut = new(sourceWorkspace.Id, targetWorkspace.Id);
+
+		// When
+		var result = ctx.Store.Dispatch(sut);
+
+		// Then
+		Assert.True(result.IsSuccessful);
+
+		Assert.Equal(targetWorkspace.Id, rootSector.MapSector.WindowWorkspaceMap[window1.Handle]);
+		Assert.Equal(targetWorkspace.Id, rootSector.MapSector.WindowWorkspaceMap[window2.Handle]);
+		Assert.Equal(targetWorkspace.Id, rootSector.MapSector.WindowWorkspaceMap[window3.Handle]);
+
+		Assert.Equal(targetWorkspace.Id, rootSector.MapSector.MonitorWorkspaceMap[monitor.Handle]);
+
+		targetWorkspace.Received(1).AddWindow(window1);
+		targetWorkspace.Received(1).AddWindow(window2);
+		targetWorkspace.Received(1).AddWindow(window3);
+	}
+}
