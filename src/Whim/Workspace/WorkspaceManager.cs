28d57c1e6775884102113645a7060c24e9f1d3e3
Isaac Daly <isaac.daly@outlook.com>
2024-05-27 13:46:34 +1000

    Port over some of WorkspaceManager

--- src/Whim/Workspace/WorkspaceManager.cs
+++ src/Whim/Workspace/WorkspaceManager.cs
@@ -2,19 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using DotNext;
 
 namespace Whim;
-
-/// <summary>
-/// Functions to trigger <see cref="WorkspaceManager"/> events, for within <see cref="Workspace"/>.
-/// </summary>
-internal record WorkspaceManagerTriggers
-{
-	public required Action<ActiveLayoutEngineChangedEventArgs> ActiveLayoutEngineChanged { get; init; }
-	public required Action<WorkspaceRenamedEventArgs> WorkspaceRenamed { get; init; }
-	public required Action<WorkspaceEventArgs> WorkspaceLayoutStarted { get; init; }
-	public required Action<WorkspaceEventArgs> WorkspaceLayoutCompleted { get; init; }
-}
 
 internal record WorkspaceToCreate(string Name, IEnumerable<CreateLeafLayoutEngine>? LayoutEngines);
 
@@ -23,12 +13,6 @@ internal class WorkspaceManager : IWorkspaceManager
 	private bool _initialized;
 	private readonly IContext _context;
 	private readonly IInternalContext _internalContext;
-	protected readonly WorkspaceManagerTriggers _triggers;
-
-	/// <summary>
-	/// The <see cref="IWorkspace"/>s stored by this manager.
-	/// </summary>
-	protected readonly List<IWorkspace> _workspaces = new();
 
 	/// <summary>
 	/// Stores the workspaces to create, when <see cref="Initialize"/> is called.
@@ -61,7 +45,11 @@ internal class WorkspaceManager : IWorkspaceManager
 		{
 			IMonitor activeMonitor = _context.MonitorManager.ActiveMonitor;
 			Logger.Debug($"Getting active workspace for monitor {activeMonitor}");
-			return _context.Butler.Pantry.GetWorkspaceForMonitor(activeMonitor) ?? _workspaces[0];
+			return _context
+				.Store.Pick(Pickers.PickWorkspaceByMonitor(activeMonitor.Handle))
+				.TryGet(out IWorkspace workspace)
+				? workspace
+				: _context.Store.Pick(new GetAllMutableWorkspacesPicker())[0];
 		}
 	}
 
@@ -71,25 +59,11 @@ internal class WorkspaceManager : IWorkspaceManager
 	{
 		_context = context;
 		_internalContext = internalContext;
-		_triggers = new WorkspaceManagerTriggers()
-		{
-			ActiveLayoutEngineChanged = (ActiveLayoutEngineChangedEventArgs e) =>
-				ActiveLayoutEngineChanged?.Invoke(this, e),
-			WorkspaceRenamed = (WorkspaceRenamedEventArgs e) => WorkspaceRenamed?.Invoke(this, e),
-			WorkspaceLayoutStarted = (WorkspaceEventArgs e) => WorkspaceLayoutStarted?.Invoke(this, e),
-			WorkspaceLayoutCompleted = (WorkspaceEventArgs e) => WorkspaceLayoutCompleted?.Invoke(this, e)
-		};
 	}
 
 	public IWorkspace? Add(string? name = null, IEnumerable<CreateLeafLayoutEngine>? createLayoutEngines = null)
 	{
-		if (_initialized)
-		{
-			return CreateWorkspace(name, createLayoutEngines);
-		}
-
-		_workspacesToCreate.Add(new(name ?? $"Workspace {_workspaces.Count + 1}", createLayoutEngines));
-		return null;
+		_context.Store.Dispatch(new AddWorkspaceTransform(name, createLayoutEngines));
 	}
 
 	public void AddProxyLayoutEngine(CreateProxyLayoutEngine proxyLayoutEngine)
@@ -98,9 +72,11 @@ internal class WorkspaceManager : IWorkspaceManager
 		_proxyLayoutEngines.Add(proxyLayoutEngine);
 	}
 
-	public bool Contains(IWorkspace workspace) => _workspaces.Contains(workspace);
+	public bool Contains(IWorkspace workspace) =>
+		_context.Store.Pick(new GetAllWorkspacesPicker()).Any(w => w.Id == workspace.Id);
 
-	public IEnumerator<IWorkspace> GetEnumerator() => _workspaces.GetEnumerator();
+	public IEnumerator<IWorkspace> GetEnumerator() =>
+		_context.Store.Pick(new GetAllMutableWorkspacesPicker()).GetEnumerator();
 
 	public void Initialize()
 	{
@@ -122,94 +98,25 @@ internal class WorkspaceManager : IWorkspaceManager
 				(idx < _workspaces.Count ? _workspaces[idx] : CreateWorkspace($"Workspace {idx + 1}"))
 				?? throw new InvalidOperationException($"Could not create workspace");
 
-			_context.Butler.Activate(workspace, monitor);
+			_context.Store.Dispatch(new ActivateWorkspaceTransform(workspace, monitor));
 			idx++;
 		}
 	}
 
-	private Workspace? CreateWorkspace(
-		string? name = null,
-		IEnumerable<CreateLeafLayoutEngine>? createLayoutEngines = null
-	)
-	{
-		CreateLeafLayoutEngine[] engineCreators = createLayoutEngines?.ToArray() ?? CreateLayoutEngines();
+	public bool Remove(IWorkspace workspace) =>
+		_context.Store.Dispatch(new RemoveWorkspaceByIdTransform(workspace.Id)).IsSuccessful;
 
-		if (engineCreators.Length == 0)
-		{
-			Logger.Error("No layout engines were provided");
-			return null;
-		}
-
-		// Create the layout engines.
-		ILayoutEngine[] layoutEngines = new ILayoutEngine[engineCreators.Length];
-		for (int i = 0; i < engineCreators.Length; i++)
-		{
-			layoutEngines[i] = engineCreators[i](new LayoutEngineIdentity());
-		}
-
-		// Set up the proxies.
-		for (int engineIdx = 0; engineIdx < engineCreators.Length; engineIdx++)
-		{
-			ILayoutEngine currentEngine = layoutEngines[engineIdx];
-			foreach (CreateProxyLayoutEngine createProxyLayoutEngineFn in _proxyLayoutEngines)
-			{
-				ILayoutEngine proxy = createProxyLayoutEngineFn(currentEngine);
-				layoutEngines[engineIdx] = proxy;
-				currentEngine = proxy;
-			}
-		}
-
-		// Create the workspace.
-		Workspace workspace =
-			new(_context, _internalContext, _triggers, name ?? $"Workspace {_workspaces.Count + 1}", layoutEngines);
-		_workspaces.Add(workspace);
-		WorkspaceAdded?.Invoke(this, new WorkspaceEventArgs() { Workspace = workspace });
-		return workspace;
-	}
-
-	public bool Remove(IWorkspace workspace)
-	{
-		Logger.Debug($"Removing workspace {workspace}");
-
-		if (_workspaces.Count - 1 < _context.MonitorManager.Length)
-		{
-			Logger.Debug($"There must be at least {_context.MonitorManager.Length} workspaces.");
-			return false;
-		}
-
-		bool wasFound = _workspaces.Remove(workspace);
-
-		if (!wasFound)
-		{
-			Logger.Debug($"Workspace {workspace} was not found");
-			return false;
-		}
-
-		_context.Butler.MergeWorkspaceWindows(workspace, _workspaces[^1]);
-		_context.Butler.Activate(_workspaces[^1]);
-		WorkspaceRemoved?.Invoke(this, new WorkspaceEventArgs() { Workspace = workspace });
-
-		return wasFound;
-	}
-
-	public bool Remove(string workspaceName)
-	{
-		Logger.Debug($"Trying to remove workspace {workspaceName}");
-
-		IWorkspace? workspace = _workspaces.Find(w => w.Name == workspaceName);
-		if (workspace == null)
-		{
-			Logger.Debug($"Workspace {workspaceName} not found");
-			return false;
-		}
-
-		return Remove(workspace);
-	}
+	public bool Remove(string workspaceName) =>
+		_context.Store.Dispatch(new RemoveWorkspaceByNameTransform(workspaceName)).IsSuccessful;
 
 	public IWorkspace? TryGet(string workspaceName)
 	{
-		Logger.Debug($"Trying to get workspace {workspaceName}");
-		return _workspaces.Find(w => w.Name == workspaceName);
+		Result<ImmutableWorkspace> result = _context.Store.Pick(new GetWorkspaceByNamePicker(workspaceName));
+		if (result.TryGet(out ImmutableWorkspace workspace))
+		{
+			return _context.Store.Pick(new GetMutableWorkspaceByIdPicker(workspace.Id));
+		}
+		return null;
 	}
 
 	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
