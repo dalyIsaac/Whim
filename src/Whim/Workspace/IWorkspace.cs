2a4f5b7456cdecc453884b2b9b604ee601509c2a
Isaac Daly <isaac.daly@outlook.com>
2024-05-27 21:58:22 +1000

    Move a bunch of ImmutableWorkspace properties to IWorkspace

--- src/Whim/Workspace/IWorkspace.cs
+++ src/Whim/Workspace/IWorkspace.cs
@@ -1,6 +1,8 @@
 global using WorkspaceId = System.Guid;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using Windows.Win32.Foundation;
 
 namespace Whim;
 
@@ -10,9 +12,46 @@ namespace Whim;
 public interface IWorkspace : IDisposable
 {
 	/// <summary>
-	/// The id of the workspace.
+	/// The unique id of the workspace.
 	/// </summary>
 	WorkspaceId Id { get; }
+
+	/// <summary>
+	/// The name of the workspace.
+	/// </summary>
+	/// <remarks>
+	/// Until the legacy <see cref="Workspace"/> implementation is removed, this is called
+	/// <c>BackingName</c> to avoid confusion with <see cref="Name"/>.
+	/// Once the legacy <see cref="Workspace"/> implementation is removed, this will be
+	/// called <c>Name</c>.
+	/// </remarks>
+	string BackingName { get; }
+
+	/// <summary>
+	/// The index of the layout engine in <see cref="LayoutEngines"/> which is currently active.
+	/// </summary>
+	int ActiveLayoutEngineIndex { get; }
+
+	/// <summary>
+	/// The index of the layout engine in <see cref="LayoutEngines"/> which was previously active.
+	/// </summary>
+	int PreviousLayoutEngineIndex { get; }
+
+	/// <summary>
+	/// The index of the last focused window in the workspace.
+	/// WARNING: When the value is 0, it means that no window is focused. Check this with <see cref="HWND.IsNull"/>.
+	/// </summary>
+	HWND LastFocusedWindowHandle { get; }
+
+	/// <summary>
+	/// All the layout engines currently in the workspace.
+	/// </summary>
+	ImmutableList<ILayoutEngine> LayoutEngines { get; }
+
+	/// <summary>
+	/// Map of windows to their <see cref="IWindowState"/>s.
+	/// </summary>
+	ImmutableDictionary<HWND, IWindowState> WindowStates { get; }
 
 	/// <summary>
 	/// The name of the workspace. When the <c>Name</c> is set, the
--- src/Whim/Workspace/Workspace.cs
+++ src/Whim/Workspace/Workspace.cs
@@ -8,46 +8,25 @@ namespace Whim;
 /// </summary>
 public sealed partial record Workspace : IWorkspace
 {
-	/// <summary>
-	/// The unique id of the workspace.
-	/// </summary>
+	/// <inheritdoc />
 	public WorkspaceId Id { get; internal init; } = WorkspaceId.NewGuid();
 
-	/// <summary>
-	/// The name of the workspace.
-	/// </summary>
-	/// <remarks>
-	/// Until the legacy <see cref="Workspace"/> implementation is removed, this is called
-	/// <c>BackingName</c> to avoid confusion with <see cref="Name"/>.
-	/// Once the legacy <see cref="Workspace"/> implementation is removed, this will be
-	/// called <c>Name</c>.
-	/// </remarks>
+	/// <inheritdoc />
 	public string BackingName { get; internal init; } = string.Empty;
 
-	/// <summary>
-	/// The index of the layout engine in <see cref="LayoutEngines"/> which is currently active.
-	/// </summary>
+	/// <inheritdoc />
 	public int ActiveLayoutEngineIndex { get; internal init; }
 
-	/// <summary>
-	/// The index of the layout engine in <see cref="LayoutEngines"/> which was previously active.
-	/// </summary>
+	/// <inheritdoc />
 	public int PreviousLayoutEngineIndex { get; internal init; }
 
-	/// <summary>
-	/// The index of the last focused window in the workspace.
-	/// WARNING: When the value is 0, it means that no window is focused. Check this with <see cref="HWND.IsNull"/>.
-	/// </summary>
+	/// <inheritdoc />
 	public HWND LastFocusedWindowHandle { get; internal init; }
 
-	/// <summary>
-	/// All the layout engines currently in the workspace.
-	/// </summary>
+	/// <inheritdoc />
 	public ImmutableList<ILayoutEngine> LayoutEngines { get; internal init; } = ImmutableList<ILayoutEngine>.Empty;
 
-	/// <summary>
-	/// Map of windows to their <see cref="IWindowState"/>s.
-	/// </summary>
+	/// <inheritdoc />
 	public ImmutableDictionary<HWND, IWindowState> WindowStates { get; internal init; } =
 		ImmutableDictionary<HWND, IWindowState>.Empty;
 
