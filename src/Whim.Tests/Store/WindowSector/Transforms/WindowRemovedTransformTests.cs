6aa6d4f0cbc392bd8e04bf85491495161a8cd69e
Isaac Daly <isaac.daly@outlook.com>
2024-06-10 17:11:39 +1000

    WindowRemovedTransformTests

--- src/Whim.Tests/Store/WindowSector/Transforms/WindowRemovedTransformTests.cs
+++ src/Whim.Tests/Store/WindowSector/Transforms/WindowRemovedTransformTests.cs
@@ -1,114 +1,111 @@
-//using System;
-//using System.Collections.Generic;
-//using DotNext;
-//using NSubstitute;
-//using Whim.TestUtils;
-//using Windows.Win32.Foundation;
-//using Xunit;
+using System.Diagnostics.CodeAnalysis;
+using DotNext;
+using NSubstitute;
+using Whim.TestUtils;
+using Windows.Win32.Foundation;
+using Xunit;
+using static Whim.TestUtils.StoreTestUtils;
 
-//namespace Whim.Tests;
+namespace Whim.Tests;
 
-//public class WindowRemovedTransformTests
-//{
-//	private static (Result<Unit>, Assert.RaisedEvent<WindowRemovedEventArgs>) AssertRaises(
-//		IContext ctx,
-//		MutableRootSector mutableRootSector,
-//		WindowRemovedTransform sut
-//	)
-//	{
-//		Result<Unit>? result = null;
-//		Assert.RaisedEvent<WindowRemovedEventArgs> ev;
+[SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope")]
+public class WindowRemovedTransformTests
+{
+	private static (Result<Unit>, Assert.RaisedEvent<WindowRemovedEventArgs>) AssertRaises(
+		IContext ctx,
+		MutableRootSector mutableRootSector,
+		WindowRemovedTransform sut
+	)
+	{
+		Result<Unit>? result = null;
+		Assert.RaisedEvent<WindowRemovedEventArgs> ev;
 
-//		ev = Assert.Raises<WindowRemovedEventArgs>(
-//			h => mutableRootSector.WindowSector.WindowRemoved += h,
-//			h => mutableRootSector.WindowSector.WindowRemoved -= h,
-//			() => result = ctx.Store.Dispatch(sut)
-//		);
+		ev = Assert.Raises<WindowRemovedEventArgs>(
+			h => mutableRootSector.WindowSector.WindowRemoved += h,
+			h => mutableRootSector.WindowSector.WindowRemoved -= h,
+			() => result = ctx.Store.Dispatch(sut)
+		);
 
-//		return (result!.Value, ev);
-//	}
+		return (result!.Value, ev);
+	}
 
-//	private static Result<Unit> AssertDoesNotRaise(
-//		IContext ctx,
-//		MutableRootSector mutableRootSector,
-//		WindowRemovedTransform sut
-//	)
-//	{
-//		Result<Unit>? result = null;
-//		CustomAssert.DoesNotRaise<WindowRemovedEventArgs>(
-//			h => mutableRootSector.WindowSector.WindowRemoved += h,
-//			h => mutableRootSector.WindowSector.WindowRemoved -= h,
-//			() => result = ctx.Store.Dispatch(sut)
-//		);
-//		return result!.Value;
-//	}
+	private static Result<Unit> AssertDoesNotRaise(
+		IContext ctx,
+		MutableRootSector mutableRootSector,
+		WindowRemovedTransform sut
+	)
+	{
+		Result<Unit>? result = null;
+		CustomAssert.DoesNotRaise<WindowRemovedEventArgs>(
+			h => mutableRootSector.WindowSector.WindowRemoved += h,
+			h => mutableRootSector.WindowSector.WindowRemoved -= h,
+			() => result = ctx.Store.Dispatch(sut)
+		);
+		return result!.Value;
+	}
 
-//	[Theory, AutoSubstituteData<StoreCustomization>]
-//	internal void WindowNotTracked(IContext ctx, MutableRootSector rootSector, IWindow window)
-//	{
-//		// Given the window is not tracked
-//		WindowRemovedTransform sut = new(window);
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void WindowNotTracked(IContext ctx, MutableRootSector rootSector, IWindow window)
+	{
+		// Given the window is not tracked
+		WindowRemovedTransform sut = new(window);
 
-//		var originalWindows = rootSector.WindowSector.Windows;
-//		var originalWindowWorkspaceMap = rootSector.MapSector.WindowWorkspaceMap;
+		var originalWindows = rootSector.WindowSector.Windows;
+		var originalWindowWorkspaceMap = rootSector.MapSector.WindowWorkspaceMap;
 
-//		// When
-//		var result = AssertDoesNotRaise(ctx, rootSector, sut);
+		// When
+		var result = AssertDoesNotRaise(ctx, rootSector, sut);
 
-//		// Then the window is not removed
-//		Assert.True(result.IsSuccessful);
-//		Assert.Same(originalWindows, rootSector.WindowSector.Windows);
-//		Assert.Same(originalWindowWorkspaceMap, rootSector.MapSector.WindowWorkspaceMap);
-//	}
+		// Then the window is not removed
+		Assert.True(result.IsSuccessful);
+		Assert.Same(originalWindows, rootSector.WindowSector.Windows);
+		Assert.Same(originalWindowWorkspaceMap, rootSector.MapSector.WindowWorkspaceMap);
+	}
 
-//	[Theory, AutoSubstituteData<StoreCustomization>]
-//	internal void WindowNotInWorkspace(IContext ctx, MutableRootSector rootSector, IWindow window)
-//	{
-//		// Given the window is inside the WindowSector but not in the workspace
-//		window.Handle.Returns((HWND)2);
-//		rootSector.WindowSector.Windows = rootSector.WindowSector.Windows.Add(window.Handle, window);
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void WindowNotInWorkspace(IContext ctx, MutableRootSector rootSector, IWindow window)
+	{
+		// Given the window is inside the WindowSector but not in the workspace
+		window.Handle.Returns((HWND)2);
+		rootSector.WindowSector.Windows = rootSector.WindowSector.Windows.Add(window.Handle, window);
 
-//		WindowRemovedTransform sut = new(window);
+		WindowRemovedTransform sut = new(window);
 
-//		var originalWindows = rootSector.WindowSector.Windows;
-//		var originalWindowWorkspaceMap = rootSector.MapSector.WindowWorkspaceMap;
+		var originalWindows = rootSector.WindowSector.Windows;
+		var originalWindowWorkspaceMap = rootSector.MapSector.WindowWorkspaceMap;
 
-//		// When
-//		var result = AssertDoesNotRaise(ctx, rootSector, sut);
+		// When
+		var result = AssertDoesNotRaise(ctx, rootSector, sut);
 
-//		// Then the window is not removed
-//		Assert.True(result.IsSuccessful);
-//		Assert.Same(originalWindows, rootSector.WindowSector.Windows);
-//		Assert.Same(originalWindowWorkspaceMap, rootSector.MapSector.WindowWorkspaceMap);
-//	}
+		// Then the window is not removed
+		Assert.True(result.IsSuccessful);
+		Assert.Same(originalWindows, rootSector.WindowSector.Windows);
+		Assert.Same(originalWindowWorkspaceMap, rootSector.MapSector.WindowWorkspaceMap);
+	}
 
-//	[Theory, AutoSubstituteData<StoreCustomization>]
-//	internal void Success(IContext ctx, MutableRootSector mutableRootSector, IWindow window, IWorkspace workspace)
-//	{
-//		// Given the window is inside the WindowSector
-//		window.Handle.Returns((HWND)2);
-//		mutableRootSector.WindowSector.Windows = mutableRootSector.WindowSector.Windows.Add(window.Handle, window);
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void Success(IContext ctx, MutableRootSector mutableRootSector, IWindow window)
+	{
+		// Given the window is tracked
+		window.Handle.Returns((HWND)2);
+		Workspace workspace = CreateWorkspace(ctx);
+		PopulateWindowWorkspaceMap(ctx, mutableRootSector, window, workspace);
 
-//		// ...and inside the MapSector
-//		Guid workspaceId = Guid.NewGuid();
-//		workspace.Id.Returns(workspaceId);
-//		ctx.WorkspaceManager.GetEnumerator().Returns(_ => new List<IWorkspace>() { workspace }.GetEnumerator());
+		mutableRootSector.MapSector.WindowWorkspaceMap = mutableRootSector.MapSector.WindowWorkspaceMap.Add(
+			window.Handle,
+			workspace.Id
+		);
 
-//		mutableRootSector.MapSector.WindowWorkspaceMap = mutableRootSector.MapSector.WindowWorkspaceMap.Add(
-//			window.Handle,
-//			workspaceId
-//		);
+		WindowRemovedTransform sut = new(window);
 
-//		WindowRemovedTransform sut = new(window);
+		// When
+		(var result, var ev) = AssertRaises(ctx, mutableRootSector, sut);
 
-//		// When
-//		(var result, var ev) = AssertRaises(ctx, mutableRootSector, sut);
+		// Then
+		Assert.True(result.IsSuccessful);
+		Assert.Equal(window, ev.Arguments.Window);
 
-//		// Then
-//		Assert.True(result.IsSuccessful);
-//		Assert.Equal(window, ev.Arguments.Window);
-
-//		Assert.Empty(mutableRootSector.WindowSector.Windows);
-//		Assert.Empty(mutableRootSector.MapSector.WindowWorkspaceMap);
-//	}
-//}
+		Assert.Empty(mutableRootSector.WindowSector.Windows);
+		Assert.Empty(mutableRootSector.MapSector.WindowWorkspaceMap);
+	}
+}
