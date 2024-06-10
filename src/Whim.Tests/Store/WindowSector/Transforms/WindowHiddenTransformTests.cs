59799088e7a13447d2e9619c30130b7b00245f8c
Isaac Daly <isaac.daly@outlook.com>
2024-06-10 21:22:17 +1000

    WindowHiddenTransformTests

--- src/Whim.Tests/Store/WindowSector/Transforms/WindowHiddenTransformTests.cs
+++ src/Whim.Tests/Store/WindowSector/Transforms/WindowHiddenTransformTests.cs
@@ -1,95 +1,82 @@
-// using System;
-// using System.Collections.Generic;
-// using DotNext;
-// using NSubstitute;
-// using NSubstitute.ReturnsExtensions;
-// using Whim.TestUtils;
-// using Windows.Win32.Foundation;
-// using Xunit;
-//
-// namespace Whim.Tests;
-//
-// public class WindowHiddenTransformTests
-// {
-// 	private static Result<Unit> AssertDoesNotRaise(
-// 		IContext ctx,
-// 		MutableRootSector mutableRootSector,
-// 		WindowHiddenTransform sut
-// 	)
-// 	{
-// 		Result<Unit>? result = null;
-//
-// 		CustomAssert.DoesNotRaise<WindowRemovedEventArgs>(
-// 			h => mutableRootSector.WindowSector.WindowRemoved += h,
-// 			h => mutableRootSector.WindowSector.WindowRemoved -= h,
-// 			() => result = ctx.Store.Dispatch(sut)
-// 		);
-//
-// 		return result!.Value;
-// 	}
-//
-// 	private static (Result<Unit>, Assert.RaisedEvent<WindowRemovedEventArgs>) AssertRaises(
-// 		IContext ctx,
-// 		MutableRootSector mutableRootSector,
-// 		WindowHiddenTransform sut
-// 	)
-// 	{
-// 		Result<Unit>? result = null;
-// 		Assert.RaisedEvent<WindowRemovedEventArgs> ev;
-//
-// 		ev = Assert.Raises<WindowRemovedEventArgs>(
-// 			h => mutableRootSector.WindowSector.WindowRemoved += h,
-// 			h => mutableRootSector.WindowSector.WindowRemoved -= h,
-// 			() => result = ctx.Store.Dispatch(sut)
-// 		);
-//
-// 		return (result!.Value, ev);
-// 	}
-//
-// 	[Theory, AutoSubstituteData<StoreCustomization>]
-// 	internal void Failed(IContext ctx, MutableRootSector mutableRootSector, IWindow window)
-// 	{
-// 		// Given
-// 		ctx.Butler.Pantry.GetMonitorForWindow(window).ReturnsNull();
-// 		WindowHiddenTransform sut = new(window);
-//
-// 		// When
-// 		var result = AssertDoesNotRaise(ctx, mutableRootSector, sut);
-//
-// 		// Then
-// 		Assert.True(result.IsSuccessful);
-// 	}
-//
-// 	[Theory, AutoSubstituteData<StoreCustomization>]
-// 	internal void Success(
-// 		IContext ctx,
-// 		MutableRootSector rootSector,
-// 		IWindow window,
-// 		IMonitor monitor,
-// 		IWorkspace workspace
-// 	)
-// 	{
-// 		// Given the window is inside the window sector,
-// 		window.Handle.Returns((HWND)2);
-// 		rootSector.WindowSector.Windows = rootSector.WindowSector.Windows.Add(window.Handle, window);
-//
-// 		// and has a monitor - window mapping
-// 		Guid workspaceId = Guid.NewGuid();
-// 		workspace.Id.Returns(workspaceId);
-//
-// 		MapSector mapSector = rootSector.MapSector;
-// 		mapSector.WindowWorkspaceMap = mapSector.WindowWorkspaceMap.SetItem(window.Handle, workspaceId);
-// 		mapSector.MonitorWorkspaceMap = mapSector.MonitorWorkspaceMap.SetItem(monitor.Handle, workspaceId);
-//
-// 		ctx.WorkspaceManager.GetEnumerator().Returns(_ => new List<IWorkspace>() { workspace }.GetEnumerator());
-//
-// 		WindowHiddenTransform sut = new(window);
-//
-// 		// When
-// 		(var result, var ev) = AssertRaises(ctx, rootSector, sut);
-//
-// 		// Then
-// 		Assert.True(result.IsSuccessful);
-// 		Assert.Equal(window, ev.Arguments.Window);
-// 	}
-// }
+using System.Diagnostics.CodeAnalysis;
+using DotNext;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using Whim.TestUtils;
+using Windows.Win32.Foundation;
+using Xunit;
+using static Whim.TestUtils.StoreTestUtils;
+
+namespace Whim.Tests;
+
+[SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope")]
+public class WindowHiddenTransformTests
+{
+	private static Result<Unit> AssertDoesNotRaise(
+		IContext ctx,
+		MutableRootSector mutableRootSector,
+		WindowHiddenTransform sut
+	)
+	{
+		Result<Unit>? result = null;
+
+		CustomAssert.DoesNotRaise<WindowRemovedEventArgs>(
+			h => mutableRootSector.WindowSector.WindowRemoved += h,
+			h => mutableRootSector.WindowSector.WindowRemoved -= h,
+			() => result = ctx.Store.Dispatch(sut)
+		);
+
+		return result!.Value;
+	}
+
+	private static (Result<Unit>, Assert.RaisedEvent<WindowRemovedEventArgs>) AssertRaises(
+		IContext ctx,
+		MutableRootSector mutableRootSector,
+		WindowHiddenTransform sut
+	)
+	{
+		Result<Unit>? result = null;
+		Assert.RaisedEvent<WindowRemovedEventArgs> ev;
+
+		ev = Assert.Raises<WindowRemovedEventArgs>(
+			h => mutableRootSector.WindowSector.WindowRemoved += h,
+			h => mutableRootSector.WindowSector.WindowRemoved -= h,
+			() => result = ctx.Store.Dispatch(sut)
+		);
+
+		return (result!.Value, ev);
+	}
+
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void Failed(IContext ctx, MutableRootSector mutableRootSector, IWindow window)
+	{
+		// Given
+		ctx.Butler.Pantry.GetMonitorForWindow(window).ReturnsNull();
+		WindowHiddenTransform sut = new(window);
+
+		// When
+		var result = AssertDoesNotRaise(ctx, mutableRootSector, sut);
+
+		// Then
+		Assert.True(result.IsSuccessful);
+	}
+
+	[Theory, AutoSubstituteData<StoreCustomization>]
+	internal void Success(IContext ctx, MutableRootSector rootSector, IWindow window, IMonitor monitor)
+	{
+		// Given the window is inside the window sector,
+		window.Handle.Returns((HWND)2);
+
+		Workspace workspace = CreateWorkspace(ctx);
+		PopulateThreeWayMap(ctx, rootSector, monitor, workspace, window);
+
+		WindowHiddenTransform sut = new(window);
+
+		// When
+		(var result, var ev) = AssertRaises(ctx, rootSector, sut);
+
+		// Then
+		Assert.True(result.IsSuccessful);
+		Assert.Equal(window, ev.Arguments.Window);
+	}
+}
