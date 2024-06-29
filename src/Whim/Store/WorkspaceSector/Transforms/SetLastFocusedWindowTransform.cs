d33e463d0cf70abfaaa14a4784de2dfbd7368de7
Isaac Daly <isaac.daly@outlook.com>
2024-06-29 20:45:09 +1000

    Prevent layouts when focusing a window

--- src/Whim/Store/WorkspaceSector/Transforms/SetLastFocusedWindowTransform.cs
+++ src/Whim/Store/WorkspaceSector/Transforms/SetLastFocusedWindowTransform.cs
@@ -5,13 +5,13 @@ namespace Whim;
 /// </summary>
 /// <param name="WorkspaceId"></param>
 /// <param name="WindowHandle"></param>
-internal record SetLastFocusedWindowTransform(Guid WorkspaceId, HWND WindowHandle)
+internal record SetLastFocusedWindowTransform(WorkspaceId WorkspaceId, HWND WindowHandle)
 	: BaseWorkspaceWindowTransform(
 		WorkspaceId,
 		WindowHandle,
 		DefaultToLastFocusedWindow: false,
 		IsWindowRequiredInWorkspace: true,
-		SkipDoLayout: false
+		SkipDoLayout: true
 	)
 {
 	private protected override Result<Workspace> WindowOperation(
