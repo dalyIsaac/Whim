66bb8411ad3da3db70f288af78bff45a0743bb3d
urob <978080+urob@users.noreply.github.com>
2024-07-09 19:54:37 -0400

    Update komorebi application rules license

--- app_rules/generate_app_rules.py
+++ app_rules/generate_app_rules.py
@@ -16,7 +16,7 @@ HEADER = """\
  *
  * MIT License
  *
- * Copyright (c) 2021 Jade Iqbal
+ * Copyright (c) 2024 Jade Iqbal
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
