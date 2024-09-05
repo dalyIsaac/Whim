37483292c3d718c1abe0eeb749ccf8a16bede404
Isaac Daly <isaac.daly@outlook.com>
2024-09-05 21:08:33 +1000

    YAML to JSON to schema POC

--- src/Whim.Json.Tests/SampleTests.cs
+++ src/Whim.Json.Tests/SampleTests.cs
@@ -1,5 +1,9 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using Whim.TestUtils;
 using Xunit;
+using Yaml2JsonNode;
+using YamlDotNet.RepresentationModel;
 
 namespace Whim.Json.Tests;
 
@@ -9,6 +13,22 @@ public class SampleTests
 	public void TestSample()
 	{
 		var schema = Sample.GetSample();
+		Assert.NotNull(schema);
+	}
+
+	[Fact]
+	public void TestSampleYaml()
+	{
+		// Read the YAML file from sample.yaml
+		using StreamReader stringReader = new("C:\\Users\\dalyisaac\\Repos\\Whim\\src\\Whim.Json.Tests\\sample.yaml");
+		YamlStream stream = [];
+
+		stream.Load(stringReader);
+		JsonNode? root = stream.ToJsonNode().First();
+		JsonElement element = JsonSerializer.Deserialize<JsonElement>(root);
+
+		// Convert the nodes to the schema.
+		var schema = Schema.FromJson(element);
 		Assert.NotNull(schema);
 	}
 }
