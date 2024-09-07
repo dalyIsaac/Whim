21ba562073f0121d44569f10b70a4b06c86bfd0d
Isaac Daly <isaac.daly@outlook.com>
2024-09-07 17:18:52 +1000

    Add parser

---
+++ src/Whim.Json/JsonParser.cs
@@ -0,0 +1,56 @@
+namespace Whim.Json;
+
+/// <summary>
+/// Parses JSON or YAML configuration files.
+/// </summary>
+public static class JsonParser
+{
+	private const string JsonConfigFileName = "whim.config.json";
+	private const string YamlConfigFileName = "whim.config.yaml";
+
+	/// <summary>
+	/// Loads and applies the declarative configuration from a JSON or YAML file.
+	/// </summary>
+	/// <param name="ctx">The <see cref="IContext"/> to operate on.</param>
+	public static void Load(IContext ctx)
+	{
+		Schema? schema = ParseJson(ctx);
+		if (schema == null)
+		{
+			return;
+		}
+
+		UpdateKeybinds(ctx, schema);
+	}
+
+	private static Schema? ParseJson(IContext ctx)
+	{
+		string jsonConfigPath = ctx.FileManager.GetWhimFileDir(JsonConfigFileName);
+		string yamlConfigPath = ctx.FileManager.GetWhimFileDir(YamlConfigFileName);
+
+		if (ctx.FileManager.FileExists(jsonConfigPath))
+		{
+			string json = ctx.FileManager.ReadAllText(jsonConfigPath);
+			return ParseJson(json);
+		}
+
+		if (ctx.FileManager.FileExists(yamlConfigPath))
+		{
+			string yaml = ctx.FileManager.ReadAllText(yamlConfigPath);
+			return ParseYaml(yaml);
+		}
+
+		return null;
+	}
+
+	private static void UpdateKeybinds(IContext ctx, Schema schema)
+	{
+		IKeybindManager keybindManager = ctx.KeybindManager;
+		keybindManager.Clear();
+
+		foreach (KeyValuePair<string, Keybind> keybind in schema.Keybinds)
+		{
+			keybindManager.SetKeybind(keybind.Key, keybind.Value);
+		}
+	}
+}
