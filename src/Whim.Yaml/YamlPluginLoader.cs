b8b2ca732f5cfd7007303807e85a7bc1912b7685
Isaac Daly <isaac.daly@outlook.com>
2024-09-10 21:46:08 +1000

    Configure the GapsPlugin and start it

---
+++ src/Whim.Yaml/YamlPluginLoader.cs
@@ -0,0 +1,53 @@
+using Corvus.Json;
+using Whim.Gaps;
+
+namespace Whim.Yaml;
+
+public static class YamlPluginLoader
+{
+	public static void LoadPlugins(IContext ctx, Schema schema)
+	{
+		LoadGapsPlugin(ctx, schema);
+	}
+
+	private static void LoadGapsPlugin(IContext ctx, Schema schema)
+	{
+		var gaps = schema.Plugins.Gaps;
+
+		if (!gaps.IsValid())
+		{
+			Logger.Debug("Gaps plugin is not valid.");
+			return;
+		}
+
+		if (gaps.IsEnabled.AsOptional() is { } isEnabled && !isEnabled)
+		{
+			Logger.Debug("Gaps plugin is not enabled.");
+			return;
+		}
+
+		GapsConfig config = new();
+
+		if (gaps.OuterGap.AsOptional() is { } outerGap)
+		{
+			config.OuterGap = (int)outerGap;
+		}
+
+		if (gaps.InnerGap.AsOptional() is { } innerGap)
+		{
+			config.InnerGap = (int)innerGap;
+		}
+
+		if (gaps.DefaultOuterDelta.AsOptional() is { } defaultOuterDelta)
+		{
+			config.DefaultOuterDelta = (int)defaultOuterDelta;
+		}
+
+		if (gaps.DefaultInnerDelta.AsOptional() is { } defaultInnerDelta)
+		{
+			config.DefaultInnerDelta = (int)defaultInnerDelta;
+		}
+
+		ctx.PluginManager.AddPlugin(new GapsPlugin(ctx, config));
+	}
+}
