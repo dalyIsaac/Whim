f1fb75c604bbb8227e748d5b97be784f72bba5c9
Isaac Daly <isaac.daly@outlook.com>
2024-09-14 22:48:06 +1000

    CommandPalette YAML tests

--- src/Whim.Yaml.Tests/YamlLoaderTests.cs
+++ src/Whim.Yaml.Tests/YamlLoaderTests.cs
@@ -107,4 +107,22 @@ public class YamlLoaderTests
 		// Then the string is converted to camel case
 		Assert.Equal(expected, result);
 	}
+
+	[Theory]
+	[InlineData("none", BackdropType.None)]
+	[InlineData("acrylic", BackdropType.Acrylic)]
+	[InlineData("acrylic_thin", BackdropType.AcrylicThin)]
+	[InlineData("mica", BackdropType.Mica)]
+	[InlineData("mica_alt", BackdropType.MicaAlt)]
+	[InlineData(" ", BackdropType.None)]
+	[InlineData("", BackdropType.None)]
+	public void ParseBackdropType(string backdropType, BackdropType expected)
+	{
+		// Given a backdrop type string
+		// When parsing it
+		BackdropType result = backdropType.ParseBackdropType();
+
+		// Then the string is converted to a backdrop type
+		Assert.Equal(expected, result);
+	}
 }
