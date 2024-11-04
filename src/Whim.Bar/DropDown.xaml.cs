aceb525cf2e16d470474998da620b14615ffd980
Isaac Daly <isaac.daly@outlook.com>
2024-11-05 00:25:04 +1100

    Docs

--- src/Whim.Bar/DropDown.xaml.cs
+++ src/Whim.Bar/DropDown.xaml.cs
@@ -6,19 +6,26 @@ using Microsoft.UI.Xaml.Input;
 
 namespace Whim.Bar;
 
-// TODO: Move this justification
-// The ComboBox doesn't easily support variable-width buttons.
-// The DropDownButton + MenuFlyout + MenuFlyoutItem doesn't have data binding.
-
+/// <summary>
+/// A custom drop-down control that uses a <see cref="RadioMenuFlyoutItem"/> for each item.
+/// This is used instead of a <see cref="ComboBox"/> because the <see cref="ComboBox"/>
+/// doesn't easily support variable-width buttons, or have data binding.
+/// </summary>
 
 public sealed partial class DropDown : UserControl
 {
+	/// <summary>
+	/// The selected item in the drop-down.
+	/// </summary>
 	public string SelectedItem
 	{
 		get => (string)GetValue(SelectedItemProperty);
 		set => SetValue(SelectedItemProperty, value);
 	}
 
+	/// <summary>
+	/// The selected item in the drop-down.
+	/// </summary>
 	public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register(
 		nameof(SelectedItem),
 		typeof(string),
@@ -26,12 +33,18 @@ public sealed partial class DropDown : UserControl
 		new PropertyMetadata("")
 	);
 
+	/// <summary>
+	/// The items to display in the drop-down.
+	/// </summary>
 	public object ItemsSource
 	{
 		get => GetValue(ItemsSourceProperty);
 		set => SetValue(ItemsSourceProperty, value);
 	}
 
+	/// <summary>
+	/// The items to display in the drop-down.
+	/// </summary>
 	public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register(
 		nameof(ItemsSource),
 		typeof(object),
@@ -39,6 +52,9 @@ public sealed partial class DropDown : UserControl
 		new PropertyMetadata(null)
 	);
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DropDown"/> class.
+	/// </summary>
 	public DropDown()
 	{
 		UIElementExtensions.InitializeComponent(this, "Whim.Bar", "DropDown");
@@ -54,6 +70,11 @@ public sealed partial class DropDown : UserControl
 	private void DropDown_GettingFocus(UIElement sender, GettingFocusEventArgs args) =>
 		OnPropertyChanged(this, ItemsSourceProperty);
 
+	/// <summary>
+	/// Rebuild the drop-down when the items source or selected item changes.
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="dp"></param>
 	private void OnPropertyChanged(DependencyObject sender, DependencyProperty dp)
 	{
 		if (ItemsSource is not IEnumerable<string> items)
@@ -77,6 +98,11 @@ public sealed partial class DropDown : UserControl
 		}
 	}
 
+	/// <summary>
+	/// Update the selected item when a radio item is clicked.
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
 	private void RadioItem_Click(object sender, RoutedEventArgs e)
 	{
 		if (sender is not RadioMenuFlyoutItem item)
